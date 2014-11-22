6ad0583c25b2a6f1289a42990b5ed560c2ad0955
clearostranslations <translations@clearfoundation.com>
2014-11-21 19:37:31 -0500

    Updated translations via Pootle

--- language/ru_RU/multiwan_lang.php
+++ language/ru_RU/multiwan_lang.php
@@ -3,20 +3,20 @@
 // http://www.clearfoundation.com/docs/developer/translations/
 
 $lang['multiwan_app_description'] = 'The Multi-WAN app allows you to connect your system to multiple Internet connections. Multi-WAN offers many benefits to environments requiring a reliable connection, including load balancing, packet segregation and automatic failover.';
-$lang['multiwan_app_name'] = 'Multi-WAN';
+$lang['multiwan_app_name'] = 'Мульти-WAN';
 $lang['multiwan_backup'] = 'Backup';
 $lang['multiwan_destination_port_rule'] = 'Destination Port Rule';
 $lang['multiwan_destination_port_rules'] = 'Destination Port Rules';
-$lang['multiwan_in_use'] = 'In Use';
-$lang['multiwan_multiwan'] = 'Multi-WAN';
-$lang['multiwan_multiwan_status'] = 'Multi-WAN Status';
-$lang['multiwan_network_status'] = 'Network Status';
-$lang['multiwan_offline'] = 'Offline';
-$lang['multiwan_online'] = 'Online';
+$lang['multiwan_in_use'] = 'В использовании';
+$lang['multiwan_multiwan'] = 'Мульти-WAN';
+$lang['multiwan_multiwan_status'] = 'Статус Мульти-WAN';
+$lang['multiwan_network_status'] = 'Статус сети';
+$lang['multiwan_offline'] = 'Выключен';
+$lang['multiwan_online'] = 'Включен';
 $lang['multiwan_primary'] = 'Primary';
 $lang['multiwan_primary_or_backup'] = 'Primary or backup';
 $lang['multiwan_source_based_route'] = 'Source-based Route';
 $lang['multiwan_source_based_routes'] = 'Source-based Routes';
-$lang['multiwan_weight'] = 'Weight';
+$lang['multiwan_weight'] = 'Авторитет';
 $lang['multiwan_weight_invalid'] = 'Multi-WAN weight is invalid.';
 $lang['multiwan_weight_out_of_range'] = 'Multi-WAN weight is out of range.';

c59a72fa184098e5f6d0975a60c8598f3ad112e6
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 16:50:35 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/yo/multiwan_lang.php
@@ -0,0 +1,22 @@
+<?php
+
+$lang['multiwan_app_description'] = 'Awọn Olona-WAN app faye gba o lati so rẹ eto to ọpọ awọn isopọ Ayelujara. Olona-WAN nfun ni ọpọlọpọ awọn anfani lati agbegbe to nilo kan gbẹkẹle asopọ, pẹlu fifuye iwontunwosi, soso ipinya ati ki o laifọwọyi failover.';
+$lang['multiwan_app_name'] = 'Multi-WAN';
+$lang['multiwan_backup'] = 'afẹyinti';
+$lang['multiwan_destination_port_rule'] = 'Nlo Port Ofin';
+$lang['multiwan_destination_port_rules'] = 'Nlo Port Ofin';
+$lang['multiwan_in_use'] = 'ni Lo';
+$lang['multiwan_mode'] = 'mode';
+$lang['multiwan_multiwan'] = 'Multi-WAN';
+$lang['multiwan_multiwan_status'] = 'Multi-WAN Ipo';
+$lang['multiwan_network_status'] = 'Network Ipo';
+$lang['multiwan_offline'] = 'aikilẹhin ti';
+$lang['multiwan_online'] = 'online';
+$lang['multiwan_primary'] = 'Primary';
+$lang['multiwan_source_based_route'] = 'Orisun-orisun Route';
+$lang['multiwan_source_based_routes'] = 'Orisun-orisun ipa';
+$lang['multiwan_standby'] = 'Duro die';
+$lang['multiwan_updating_status'] = 'Multi-WAN ti wa ni mimu ipo alaye, yi yoo nikan ya kan diẹ aaya ...';
+$lang['multiwan_weight'] = 'àdánù';
+$lang['multiwan_weight_invalid'] = 'Multi-WAN àdánù jẹ invalid.';
+$lang['multiwan_weight_out_of_range'] = 'Olona-WAN àdánù jẹ jade ti ibiti o.';

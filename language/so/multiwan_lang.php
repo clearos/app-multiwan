a5b844e0594129dd3db929e11f56d1461c33bc71
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 16:48:08 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/so/multiwan_lang.php
@@ -0,0 +1,22 @@
+<?php
+
+$lang['multiwan_app_description'] = 'The app Multi-WAN kuu ogolaanayaa inaad in ay ku xidhmaan aad nidaamka si xiriir Internet badan. Multi-WAN bixisaa faa&#39;iidooyin badan degaanadooda u baahan xiriir ah oo lagu kalsoonaan karo, oo ay ku jiraan miisaamid load, kalasaar baakadda iyo failover automatic.';
+$lang['multiwan_app_name'] = 'Multi-WAN';
+$lang['multiwan_backup'] = 'kaabta';
+$lang['multiwan_destination_port_rule'] = 'Destination Port Rule';
+$lang['multiwan_destination_port_rules'] = 'Destination Port Rules';
+$lang['multiwan_in_use'] = 'In Isticmaalka';
+$lang['multiwan_mode'] = 'Mode';
+$lang['multiwan_multiwan'] = 'Multi-WAN';
+$lang['multiwan_multiwan_status'] = 'Multi-WAN Status';
+$lang['multiwan_network_status'] = 'Status Network';
+$lang['multiwan_offline'] = 'offline';
+$lang['multiwan_online'] = 'Online';
+$lang['multiwan_primary'] = 'Primary';
+$lang['multiwan_source_based_route'] = 'Source-ku-saleysan Route';
+$lang['multiwan_source_based_routes'] = 'Wadooyinka Source-ku saleysan';
+$lang['multiwan_standby'] = 'daaran';
+$lang['multiwan_updating_status'] = 'Multi-WAN waxaa la xariirta macluumaadka xaaladda, waxay qaadan doontaa oo keliya xoogaa ilbiriqsiyo ah ...';
+$lang['multiwan_weight'] = 'Miisaanka';
+$lang['multiwan_weight_invalid'] = 'miisaanka Multi-WAN waa baadil.';
+$lang['multiwan_weight_out_of_range'] = 'miisaanka Multi-WAN waa out of kala duwan.';

3c8b89d97eda1c1aca3dc3a6fb54a079f064ac95
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 16:43:39 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/is/multiwan_lang.php
@@ -0,0 +1,22 @@
+<?php
+
+$lang['multiwan_app_description'] = 'The Multi-WAN app gerir þér kleift að tengja vélina við margar tengingar Internet. Multi-WAN býður upp á marga kosti við umhverfi sem þurfa áreiðanlega tengingu, þar á meðal hlaða jafnvægi, Packet aðskilnaðar og sjálfvirka failover.';
+$lang['multiwan_app_name'] = 'Multi-WAN';
+$lang['multiwan_backup'] = 'Backup';
+$lang['multiwan_destination_port_rule'] = 'Áfangastaður Port Regla';
+$lang['multiwan_destination_port_rules'] = 'Destination Port Reglur';
+$lang['multiwan_in_use'] = 'Í notkun';
+$lang['multiwan_mode'] = 'Mode';
+$lang['multiwan_multiwan'] = 'Multi-WAN';
+$lang['multiwan_multiwan_status'] = 'Multi-WAN Staða';
+$lang['multiwan_network_status'] = 'Network Status';
+$lang['multiwan_offline'] = 'offline';
+$lang['multiwan_online'] = 'Online';
+$lang['multiwan_primary'] = 'Primary';
+$lang['multiwan_source_based_route'] = 'Heimild byggir Route';
+$lang['multiwan_source_based_routes'] = 'Heimild byggir Routes';
+$lang['multiwan_standby'] = 'Standby';
+$lang['multiwan_updating_status'] = 'Multi-WAN er að uppfæra upplýsingar um stöðu, þetta mun aðeins taka nokkrar sekúndur ...';
+$lang['multiwan_weight'] = 'þyngd';
+$lang['multiwan_weight_invalid'] = 'Multi-WAN þyngd er ógilt.';
+$lang['multiwan_weight_out_of_range'] = 'Multi-WAN þyngd er utan sviðs.';

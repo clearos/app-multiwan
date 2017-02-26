ab22c6a09cfad1db1d0ef5a66c510dbd412556e5
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 16:41:08 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/zh/multiwan_lang.php
@@ -0,0 +1,22 @@
+<?php
+
+$lang['multiwan_app_description'] = '多WAN应用程序允许您将系统连接到多个互联网连接。 多WAN提供给需要可靠的连接，包括负载均衡，数据包隔离和自动故障转移的环境中有很多好处。';
+$lang['multiwan_app_name'] = 'Multi-WAN';
+$lang['multiwan_backup'] = '备用';
+$lang['multiwan_destination_port_rule'] = '目的端口规则';
+$lang['multiwan_destination_port_rules'] = '目的端口规则';
+$lang['multiwan_in_use'] = '正在使用';
+$lang['multiwan_mode'] = '模式';
+$lang['multiwan_multiwan'] = 'Multi-WAN';
+$lang['multiwan_multiwan_status'] = '多WAN状态';
+$lang['multiwan_network_status'] = '网络状态';
+$lang['multiwan_offline'] = '离线';
+$lang['multiwan_online'] = '线上';
+$lang['multiwan_primary'] = '主';
+$lang['multiwan_source_based_route'] = '源路由基础';
+$lang['multiwan_source_based_routes'] = '基于源的路由';
+$lang['multiwan_standby'] = '支持';
+$lang['multiwan_updating_status'] = '多WAN正在更新状态信息，这将只需要几秒钟...';
+$lang['multiwan_weight'] = '重量';
+$lang['multiwan_weight_invalid'] = '多WAN重量是无效的。';
+$lang['multiwan_weight_out_of_range'] = '多WAN重量超出范围。';

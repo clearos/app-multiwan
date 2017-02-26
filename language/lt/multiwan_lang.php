7623357c562e3a22d5529e9dbfab0e35286cf96a
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 16:45:01 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/lt/multiwan_lang.php
@@ -0,0 +1,22 @@
+<?php
+
+$lang['multiwan_app_description'] = 'Multi-WAN programa leidžia jums prijungti savo sistemą su keliais interneto ryšį. "Multi-WAN siūlo daug naudos aplinkoje reikia patikimą ryšį, įskaitant apkrovos balansavimo, plastikiniai segregacijos ir automatinio perjungimas.';
+$lang['multiwan_app_name'] = '"Multi-WAN';
+$lang['multiwan_backup'] = 'Atsarginė';
+$lang['multiwan_destination_port_rule'] = 'Paskirties uosto taisyklė';
+$lang['multiwan_destination_port_rules'] = 'Paskirties uosto Taisyklės';
+$lang['multiwan_in_use'] = 'Vartojamas';
+$lang['multiwan_mode'] = 'režimas';
+$lang['multiwan_multiwan'] = '"Multi-WAN';
+$lang['multiwan_multiwan_status'] = '"Multi-WAN Būsena';
+$lang['multiwan_network_status'] = 'Tinklo būklė';
+$lang['multiwan_offline'] = 'Atsijungęs';
+$lang['multiwan_online'] = 'Prisijunges';
+$lang['multiwan_primary'] = 'pagrindinis';
+$lang['multiwan_source_based_route'] = 'Šaltinis pagrindu Maršrutas';
+$lang['multiwan_source_based_routes'] = 'Šaltinis pagrindu Maršrutai';
+$lang['multiwan_standby'] = 'rezervinis';
+$lang['multiwan_updating_status'] = '"Multi-WAN atnaujina informaciją apie būseną, tai užtruks tik kelias sekundes ...';
+$lang['multiwan_weight'] = 'svoris';
+$lang['multiwan_weight_invalid'] = '"Multi-WAN svoris yra neteisingas.';
+$lang['multiwan_weight_out_of_range'] = '"Multi-WAN svoris yra už diapazono ribų.';

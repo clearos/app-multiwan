239c4fd9b279cdcd4232d13abd4b0157e3dc57ee
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 16:49:13 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/tg/multiwan_lang.php
@@ -0,0 +1,22 @@
+<?php
+
+$lang['multiwan_app_description'] = 'Дар барнома Мулти-WAN ба шумо имкон медиҳад пайваст системаи шумо ба пайвастшавии интернет сершумор. Мулти-WAN пешниҳод манфиатҳои зиёде ба муҳити талаб пайвасти боэътимод, аз ҷумла баробарсозии сарборӣ, сегрегатсияи маҷмӯъи ва failover автоматии.';
+$lang['multiwan_app_name'] = 'Мулти-WAN';
+$lang['multiwan_backup'] = 'кадани';
+$lang['multiwan_destination_port_rule'] = 'Самт Порт-Қоидаи';
+$lang['multiwan_destination_port_rules'] = 'Самт Қоидаҳои Порт-';
+$lang['multiwan_in_use'] = 'Дар истифода';
+$lang['multiwan_mode'] = 'ҳолати';
+$lang['multiwan_multiwan'] = 'Мулти-WAN';
+$lang['multiwan_multiwan_status'] = 'Мулти-WAN Статуси';
+$lang['multiwan_network_status'] = 'Статуси шабакаи';
+$lang['multiwan_offline'] = 'Ғоиб';
+$lang['multiwan_online'] = 'онлайн';
+$lang['multiwan_primary'] = 'ибтидоии';
+$lang['multiwan_source_based_route'] = 'Дар асоси Сарчашма Хатсайр';
+$lang['multiwan_source_based_routes'] = 'Хатсайрҳои асоси Манбаъ';
+$lang['multiwan_standby'] = 'Омода';
+$lang['multiwan_updating_status'] = 'Мултисектор-WAN навсозии аст, иттилооти њолати, ин танҳо як чанд сония мегирад ...';
+$lang['multiwan_weight'] = 'тарозу';
+$lang['multiwan_weight_invalid'] = 'вазни Мулти-WAN нодуруст аст.';
+$lang['multiwan_weight_out_of_range'] = 'Мулти-WAN вазни аст, берун аз доираи.';

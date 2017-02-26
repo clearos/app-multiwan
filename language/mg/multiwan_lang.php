d76880a3bcf45d07418abf06cc6e557d5c3afca6
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 16:45:17 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/mg/multiwan_lang.php
@@ -0,0 +1,22 @@
+<?php
+
+$lang['multiwan_app_description'] = 'Ny Multi-WAN rindrambaiko mamela anao hifandray ny rafitra ny maro ny fifandraisana aterineto. Multi-WAN dia manolotra tombontsoa maro ny tontolo azo antoka mitaky ny fifandraisana, anisan&#39;izany ny mampifandanja ny entana, fonosana fanavakavahana sy mandeha ho azy failover.';
+$lang['multiwan_app_name'] = 'Multi-WAN';
+$lang['multiwan_backup'] = 'nomaniny ho solon&#39;izay';
+$lang['multiwan_destination_port_rule'] = 'Destination Port Fitsipika';
+$lang['multiwan_destination_port_rules'] = 'Destination Port Fitsipika';
+$lang['multiwan_in_use'] = 'In Use';
+$lang['multiwan_mode'] = 'Mode';
+$lang['multiwan_multiwan'] = 'Multi-WAN';
+$lang['multiwan_multiwan_status'] = 'Multi-WAN Status';
+$lang['multiwan_network_status'] = 'Network Status';
+$lang['multiwan_offline'] = 'Offline';
+$lang['multiwan_online'] = 'Online';
+$lang['multiwan_primary'] = 'Kilonga';
+$lang['multiwan_source_based_route'] = 'Source-monina Route';
+$lang['multiwan_source_based_routes'] = 'Source-monina Routes';
+$lang['multiwan_standby'] = 'Miato';
+$lang['multiwan_updating_status'] = 'Multi-WAN no fanavaozana sata vaovao, izany dia vitsy ihany no haka ny segondra ...';
+$lang['multiwan_weight'] = 'Weight';
+$lang['multiwan_weight_invalid'] = 'Multi-WAN lanjany dia tsy manan-kery.';
+$lang['multiwan_weight_out_of_range'] = 'Multi-WAN lanjan&#39;ny dia avy isan-karazany.';

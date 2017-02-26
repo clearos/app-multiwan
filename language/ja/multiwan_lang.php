789271d2761ab847e23cddb21698d1f16a7295df
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 16:43:55 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/ja/multiwan_lang.php
@@ -0,0 +1,22 @@
+<?php
+
+$lang['multiwan_app_description'] = 'マルチWANのアプリでは、複数のインターネット接続にあなたのシステムを接続することができます。 マルチWANロードバランシング、パケットの分離と自動フェイルオーバーを含む信頼性の高い接続を必要とする環境に多くのメリットを提供しています。';
+$lang['multiwan_app_name'] = 'マルチWAN';
+$lang['multiwan_backup'] = 'バックアップ';
+$lang['multiwan_destination_port_rule'] = '宛先ポートルール';
+$lang['multiwan_destination_port_rules'] = '宛先ポートの規則';
+$lang['multiwan_in_use'] = '使用中で';
+$lang['multiwan_mode'] = 'モード';
+$lang['multiwan_multiwan'] = 'マルチWAN';
+$lang['multiwan_multiwan_status'] = 'マルチWANステータス';
+$lang['multiwan_network_status'] = 'ネットワークの状態';
+$lang['multiwan_offline'] = 'オフライン';
+$lang['multiwan_online'] = 'オンライン';
+$lang['multiwan_primary'] = 'プライマリー';
+$lang['multiwan_source_based_route'] = 'ソースベースのルート';
+$lang['multiwan_source_based_routes'] = 'ソースベースのルート';
+$lang['multiwan_standby'] = '待機する';
+$lang['multiwan_updating_status'] = 'マルチWANは、ステータス情報を更新している、これはほんの数秒かかります...';
+$lang['multiwan_weight'] = '重量';
+$lang['multiwan_weight_invalid'] = 'マルチWAN重量は無効です。';
+$lang['multiwan_weight_out_of_range'] = 'マルチWAN重量は範囲外です。';

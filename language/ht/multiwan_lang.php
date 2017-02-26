8549f0c13d7ee31df87ff74c8e61ece6e4cf2828
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 16:42:43 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/ht/multiwan_lang.php
@@ -0,0 +1,22 @@
+<?php
+
+$lang['multiwan_app_description'] = 'App a Multi-WAN pèmèt ou konekte sistèm ou a koneksyon entènèt miltip. Multi-WAN ofri anpil benefis ou yo ba anviwònman ki mande yon koneksyon serye, ki gen ladan balanse chaj, pake segregasyon ak fayove otomatik yo.';
+$lang['multiwan_app_name'] = 'Multi-WAN';
+$lang['multiwan_backup'] = 'sovgad';
+$lang['multiwan_destination_port_rule'] = 'Destinasyon Port Règ';
+$lang['multiwan_destination_port_rules'] = 'Destinasyon Règ Port';
+$lang['multiwan_in_use'] = 'nan Sèvi ak';
+$lang['multiwan_mode'] = 'Mode';
+$lang['multiwan_multiwan'] = 'Multi-WAN';
+$lang['multiwan_multiwan_status'] = 'Multi-WAN Ki dènye nouvèl';
+$lang['multiwan_network_status'] = 'Rezo Ki dènye nouvèl';
+$lang['multiwan_offline'] = 'Offline';
+$lang['multiwan_online'] = 'Online';
+$lang['multiwan_primary'] = 'prensipal';
+$lang['multiwan_source_based_route'] = 'Sous ki baze sou Route';
+$lang['multiwan_source_based_routes'] = 'Sous ki baze sou Wout';
+$lang['multiwan_standby'] = 'sibstiti';
+$lang['multiwan_updating_status'] = 'Multi-WAN se à enfòmasyon estati, sa a pral sèlman pran yon kèk segond ...';
+$lang['multiwan_weight'] = 'pwa';
+$lang['multiwan_weight_invalid'] = 'Multi-WAN pwa se valab.';
+$lang['multiwan_weight_out_of_range'] = 'Multi-WAN pwa se soti nan pòte.';

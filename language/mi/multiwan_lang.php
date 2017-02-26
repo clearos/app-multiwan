5536c89ae5f5bf38ef8b4216d87833b474abfcc9
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 16:45:26 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/mi/multiwan_lang.php
@@ -0,0 +1,22 @@
+<?php
+
+$lang['multiwan_app_description'] = 'Te taupānga Maha-wan taea ki a koe te hono tō pūnaha ki hononga Ipurangi maha. Maha-wan tuku maha painga ki taiao tono he hononga pono, tae kawenga whärite, pākete Pharoah me te hinga aunoa.';
+$lang['multiwan_app_name'] = 'Maha-wan';
+$lang['multiwan_backup'] = 'Pūrua';
+$lang['multiwan_destination_port_rule'] = 'Ūnga Ture Port';
+$lang['multiwan_destination_port_rules'] = 'Ūnga Ture Tauranga';
+$lang['multiwan_in_use'] = 'i roto i te Whakamahi';
+$lang['multiwan_mode'] = 'aratau';
+$lang['multiwan_multiwan'] = 'Maha-wan';
+$lang['multiwan_multiwan_status'] = 'Maha-wan Tūnga';
+$lang['multiwan_network_status'] = 'Tūnga whatunga';
+$lang['multiwan_offline'] = 'tuimotu';
+$lang['multiwan_online'] = 'online';
+$lang['multiwan_primary'] = 'Paraimere';
+$lang['multiwan_source_based_route'] = 'hāngai Pūtake-Ararere';
+$lang['multiwan_source_based_routes'] = 'Routes hāngai Pūtake-';
+$lang['multiwan_standby'] = 'tūtatari';
+$lang['multiwan_updating_status'] = 'Kei te whakahōu Maha-wan mōhiohio tūnga, tenei ka tango i te hēkona ruarua anake ...';
+$lang['multiwan_weight'] = 'Taumaha';
+$lang['multiwan_weight_invalid'] = 'He muhu Maha-wan taimaha.';
+$lang['multiwan_weight_out_of_range'] = 'He Maha-wan taimaha i roto i te awhe.';

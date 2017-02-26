3de196e2e02055106096da306f93aaeaf351ffe5
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 16:47:43 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/si/multiwan_lang.php
@@ -0,0 +1,22 @@
+<?php
+
+$lang['multiwan_app_description'] = 'මෙම බහු-WAN යෙදුම ඔබ බොහෝ අන්තර්ජාල සම්බන්ධතා සදහා ඔබගේ පද්ධතිය සම්බන්ධ කිරීමට ඉඩ දෙයි. බහු-WAN බල තුලනාත්මක, පැකට් වෙන් කිරිම් ස්වයංක්රීය වකාලන ඇතුළු විශ්වසනීය සම්බන්ධතාවයක්, අවශ්ය පරිසරයක් බොහෝ ප්රතිලාභ ලබා දෙයි.';
+$lang['multiwan_app_name'] = 'බහු-WAN';
+$lang['multiwan_backup'] = 'බැකප්';
+$lang['multiwan_destination_port_rule'] = 'ගමනාන්තය වරාය පාලනය';
+$lang['multiwan_destination_port_rules'] = 'ගමනාන්තය වරාය රීති';
+$lang['multiwan_in_use'] = 'භාවිතා කර';
+$lang['multiwan_mode'] = 'මාදිලිය';
+$lang['multiwan_multiwan'] = 'බහු-WAN';
+$lang['multiwan_multiwan_status'] = 'බහු-WAN තත්ත්වය';
+$lang['multiwan_network_status'] = 'ජාල තත්ත්වය';
+$lang['multiwan_offline'] = 'නොබැඳි';
+$lang['multiwan_online'] = 'ඔන්ලයින්';
+$lang['multiwan_primary'] = 'ප්රාථමික';
+$lang['multiwan_source_based_route'] = 'මූලාශ්රය මත පදනම් මාර්ගය';
+$lang['multiwan_source_based_routes'] = 'මූලාශ්රය මත පදනම් මාර්ග';
+$lang['multiwan_standby'] = 'සුදානමින් සිටීම';
+$lang['multiwan_updating_status'] = 'බහු-WAN මෙම පමණක් තත්පර කිහිපයක් ගත වනු ඇති තත්ත්වය තොරතුරු යාවත්කාලීන කිරීම වේ ...';
+$lang['multiwan_weight'] = 'සිරුරේ බර';
+$lang['multiwan_weight_invalid'] = 'බහු-WAN බර වලංගු නැත.';
+$lang['multiwan_weight_out_of_range'] = 'බහු-WAN බර පරාසය වේ.';

2bc91af6b3146d22b91d7363ddea7201a4fcd02e
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 16:39:50 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/hy/multiwan_lang.php
@@ -0,0 +1,22 @@
+<?php
+
+$lang['multiwan_app_description'] = 'The Multi-WAN ծրագիրը թույլ է տալիս Ձեզ միացնել ձեր համակարգը բազմաթիվ ինտերնետ կապի. Multi-WAN առաջարկում է բազմաթիվ օգուտներ է միջավայրերում պահանջում է հուսալի կապ, այդ թվում բեռի հավասարակշռում, փաթեթ խտրականությունը եւ ավտոմատ failover:';
+$lang['multiwan_app_name'] = 'Multi-WAN';
+$lang['multiwan_backup'] = 'պահուստ';
+$lang['multiwan_destination_port_rule'] = 'Destination Port Կանոն';
+$lang['multiwan_destination_port_rules'] = 'Նշանակման վայրի նավահանգիստ Օրենքները';
+$lang['multiwan_in_use'] = 'Գործածելով';
+$lang['multiwan_mode'] = 'ռեժիմ';
+$lang['multiwan_multiwan'] = 'Multi-WAN';
+$lang['multiwan_multiwan_status'] = 'Multi-WAN կարգավիճակը';
+$lang['multiwan_network_status'] = 'Ցանցի կարգավիճակը';
+$lang['multiwan_offline'] = 'Offline';
+$lang['multiwan_online'] = 'առցանց';
+$lang['multiwan_primary'] = 'հիմնական';
+$lang['multiwan_source_based_route'] = 'Աղբյուր վրա հիմնված Route';
+$lang['multiwan_source_based_routes'] = 'Աղբյուրը վրա հիմնված Routes';
+$lang['multiwan_standby'] = 'հենարան';
+$lang['multiwan_updating_status'] = 'Multi-WAN է թարմացնում կարգավիճակ տեղեկությունները, դա կլինի միայն վերցնել մի քանի վայրկյան ...';
+$lang['multiwan_weight'] = 'քաշ';
+$lang['multiwan_weight_invalid'] = 'Multi-WAN քաշը անվավեր է:';
+$lang['multiwan_weight_out_of_range'] = 'Multi-WAN քաշը դուրս Լեռնաշղթայի.';

ea39312ef6f9afeedfb7d1bcc4260d7fe69de2a2
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 16:41:47 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/et/multiwan_lang.php
@@ -0,0 +1,22 @@
+<?php
+
+$lang['multiwan_app_description'] = 'Multi-WAN rakendus võimaldab teil ühendada oma süsteemi mitu Interneti-ühendusi. Multi-WAN pakub mitmeid eeliseid keskkonnas vajavad usaldusväärne ühendus, sh koormuse tasakaalustamine, pakettaknad segregatsiooni ja automaatne tõrkesiire.';
+$lang['multiwan_app_name'] = 'Multi-WAN';
+$lang['multiwan_backup'] = 'backup';
+$lang['multiwan_destination_port_rule'] = 'Sihtkoht Port reegel';
+$lang['multiwan_destination_port_rules'] = 'Sihtkoht Port reeglid';
+$lang['multiwan_in_use'] = 'Kasutusel';
+$lang['multiwan_mode'] = 'viis';
+$lang['multiwan_multiwan'] = 'Multi-WAN';
+$lang['multiwan_multiwan_status'] = 'Multi-WAN staatus';
+$lang['multiwan_network_status'] = 'võrgu olek';
+$lang['multiwan_offline'] = 'offline';
+$lang['multiwan_online'] = 'Hetkel';
+$lang['multiwan_primary'] = 'esmane';
+$lang['multiwan_source_based_route'] = 'Allikas põhinev Route';
+$lang['multiwan_source_based_routes'] = 'Allikas põhinev Routes';
+$lang['multiwan_standby'] = 'Ootel';
+$lang['multiwan_updating_status'] = 'Multi-WAN uuendab oleku kohta, see võtab ainult paar sekundit ...';
+$lang['multiwan_weight'] = 'kaal';
+$lang['multiwan_weight_invalid'] = 'Multi-WAN kaal on kehtetu.';
+$lang['multiwan_weight_out_of_range'] = 'Multi-WAN kaal on levist väljas.';

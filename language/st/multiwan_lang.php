1a3a7a7b2b0faca355d1c21dfcaaf96a0ff53f32
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 16:48:16 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/st/multiwan_lang.php
@@ -0,0 +1,22 @@
+<?php
+
+$lang['multiwan_app_description'] = 'The app e Multi-Wan lumella u ho hokahanya hao tsamaiso ho dikgokelo multiple Internet. Multi-Wan fana melemo e mengata libakeng tse hlokang e mabapi e ka tšeptjoang, tse akarelletsang mojaro botsitso, pakete segregation le jarolla ka ho iketsa failover.';
+$lang['multiwan_app_name'] = 'Multi-Wan';
+$lang['multiwan_backup'] = 'bekapo';
+$lang['multiwan_destination_port_rule'] = 'Eang teng Port Rule';
+$lang['multiwan_destination_port_rules'] = 'Eang teng Port Rules';
+$lang['multiwan_in_use'] = 'ha le Sebelisa';
+$lang['multiwan_mode'] = 'Mode';
+$lang['multiwan_multiwan'] = 'Multi-Wan';
+$lang['multiwan_multiwan_status'] = 'Multi-Wan Status';
+$lang['multiwan_network_status'] = 'Status Network';
+$lang['multiwan_offline'] = 'Offline';
+$lang['multiwan_online'] = 'Online';
+$lang['multiwan_primary'] = 'ka sehloohong';
+$lang['multiwan_source_based_route'] = 'Mohloli o moholo e thehiloeng Route';
+$lang['multiwan_source_based_routes'] = 'Mohloli o moholo e thehiloeng Tselana';
+$lang['multiwan_standby'] = 'Ema haufi';
+$lang['multiwan_updating_status'] = 'Multi-Wan e go tlhabololwa boemo boitsebiso bo eketsehileng, sena se tla feela nka metsotswana e mmalwa ...';
+$lang['multiwan_weight'] = 'Weight';
+$lang['multiwan_weight_invalid'] = 'Multi-Wan boima ha o sebetse.';
+$lang['multiwan_weight_out_of_range'] = 'Multi-Wan boima o tsoa fihlellehang.';

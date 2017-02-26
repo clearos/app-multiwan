6aac9d3988a7b901ab04e9d0c7c9e04ecb363c7a
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 16:47:27 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/ru/multiwan_lang.php
@@ -0,0 +1,22 @@
+<?php
+
+$lang['multiwan_app_description'] = 'Приложение Multi-WAN позволяет подключать систему к нескольким интернет-соединениям. Multi-WAN предлагает множество преимуществ для задач, требующих надежного подключения, в том числе балансировка нагрузки, пакетов сегрегации и автоматического перехода на другой ресурс.';
+$lang['multiwan_app_name'] = 'Multi-WAN';
+$lang['multiwan_backup'] = 'Резервное копирование';
+$lang['multiwan_destination_port_rule'] = 'Пункт назначения Правило Порт';
+$lang['multiwan_destination_port_rules'] = 'Направление Правила порта';
+$lang['multiwan_in_use'] = 'В использовании';
+$lang['multiwan_mode'] = 'Режим';
+$lang['multiwan_multiwan'] = 'Multi-WAN';
+$lang['multiwan_multiwan_status'] = 'Multi-WAN Status';
+$lang['multiwan_network_status'] = 'Состояние сети';
+$lang['multiwan_offline'] = 'Не в сети';
+$lang['multiwan_online'] = 'В сети';
+$lang['multiwan_primary'] = 'первичная';
+$lang['multiwan_source_based_route'] = 'Источник на основе маршрута';
+$lang['multiwan_source_based_routes'] = 'Источник на основе маршрутов';
+$lang['multiwan_standby'] = 'Ожидание';
+$lang['multiwan_updating_status'] = 'Multi-WAN обновляет информацию о состоянии, это займет всего несколько секунд ...';
+$lang['multiwan_weight'] = 'вес';
+$lang['multiwan_weight_invalid'] = 'вес Multi-WAN недействителен.';
+$lang['multiwan_weight_out_of_range'] = 'Multi-WAN вес находится вне диапазона.';

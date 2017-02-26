ddaf9b825d52983dfb9b171356c27de56376ba73
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 16:48:32 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/su/multiwan_lang.php
@@ -0,0 +1,22 @@
+<?php
+
+$lang['multiwan_app_description'] = 'Aplikasi Multi-Wan ngidinan Anjeun pikeun nyambung sistim Anjeun ka sababaraha koneksi Internet. Multi-Wan nawarkeun loba mangpaat pikeun lingkungan merlukeun sambungan dipercaya, kaasup beban balancing, packet segregation jeung failover otomatis.';
+$lang['multiwan_app_name'] = 'Multi-Wan';
+$lang['multiwan_backup'] = 'Nyadangkeun';
+$lang['multiwan_destination_port_rule'] = 'Tujuan Port Aturan';
+$lang['multiwan_destination_port_rules'] = 'Tujuan Aturan Port';
+$lang['multiwan_in_use'] = 'dina Pamakéan';
+$lang['multiwan_mode'] = 'mode';
+$lang['multiwan_multiwan'] = 'Multi-Wan';
+$lang['multiwan_multiwan_status'] = 'Multi-Wan Status';
+$lang['multiwan_network_status'] = 'Status Network';
+$lang['multiwan_offline'] = 'offline';
+$lang['multiwan_online'] = 'online';
+$lang['multiwan_primary'] = 'utama';
+$lang['multiwan_source_based_route'] = 'Sumber basis Rute';
+$lang['multiwan_source_based_routes'] = 'Rute basis sumber';
+$lang['multiwan_standby'] = 'Antosan';
+$lang['multiwan_updating_status'] = 'Multi-Wan ieu ngamutahirkeun inpo status, ieu ngan bakal mawa sababaraha detik ...';
+$lang['multiwan_weight'] = 'beurat';
+$lang['multiwan_weight_invalid'] = 'beurat Multi-Wan téh sah.';
+$lang['multiwan_weight_out_of_range'] = 'Multi-Wan beurat kaluar ti jangkauan.';

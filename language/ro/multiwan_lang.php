f0a6725f1970fc6e97c59749d8d9b9b56d9d5b83
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 16:47:19 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/ro/multiwan_lang.php
@@ -0,0 +1,22 @@
+<?php
+
+$lang['multiwan_app_description'] = 'Aplicația Multi-WAN vă permite să conectați sistemul la conexiuni multiple de Internet. Multi-WAN ofera multe beneficii pentru medii care necesită o conexiune fiabilă, inclusiv dispozitive de echilibrare a sarcinii, de pachete de segregare și failover automată.';
+$lang['multiwan_app_name'] = 'Multi-WAN';
+$lang['multiwan_backup'] = 'Backup';
+$lang['multiwan_destination_port_rule'] = 'Portul de destinație Regula';
+$lang['multiwan_destination_port_rules'] = 'Reguli de portul de destinație';
+$lang['multiwan_in_use'] = 'In folosinta';
+$lang['multiwan_mode'] = 'mod';
+$lang['multiwan_multiwan'] = 'Multi-WAN';
+$lang['multiwan_multiwan_status'] = 'Multi-WAN Stare';
+$lang['multiwan_network_status'] = 'Stare rețea';
+$lang['multiwan_offline'] = 'Deconectat';
+$lang['multiwan_online'] = 'Pe net';
+$lang['multiwan_primary'] = 'Primar';
+$lang['multiwan_source_based_route'] = '-Source bazate pe ruta';
+$lang['multiwan_source_based_routes'] = 'Rutele bazate pe surse';
+$lang['multiwan_standby'] = 'Așteptare';
+$lang['multiwan_updating_status'] = 'Multi-WAN este actualizarea informațiilor de stare, acest lucru va dura doar câteva secunde ...';
+$lang['multiwan_weight'] = 'Greutate';
+$lang['multiwan_weight_invalid'] = 'Greutate multi-WAN nu este validă.';
+$lang['multiwan_weight_out_of_range'] = 'Greutate multi-WAN este în afara intervalului.';

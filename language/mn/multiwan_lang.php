01772af63f3e37e153a31779efbafbccf5178500
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 16:45:50 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/mn/multiwan_lang.php
@@ -0,0 +1,22 @@
+<?php
+
+$lang['multiwan_app_description'] = 'Олон WAN апп танд олон Интернэт холболтыг таны системийг холбох боломжийг олгодог. Олон WAN найдвартай холболт, ачаалал тэгш, пакет ангилах, автомат шилжилтийг зэрэг шаардлагатай орчинд олон давуу талтай.';
+$lang['multiwan_app_name'] = 'Олон WAN';
+$lang['multiwan_backup'] = 'Нөөцлөх';
+$lang['multiwan_destination_port_rule'] = 'Destination Порт дүрэм';
+$lang['multiwan_destination_port_rules'] = 'Destination Порт журам';
+$lang['multiwan_in_use'] = 'Ашиглагдаж байгаа';
+$lang['multiwan_mode'] = 'Горим';
+$lang['multiwan_multiwan'] = 'Олон WAN';
+$lang['multiwan_multiwan_status'] = 'Олон WAN байдал';
+$lang['multiwan_network_status'] = 'Сүлжээний байдал';
+$lang['multiwan_offline'] = 'Offline';
+$lang['multiwan_online'] = 'Онлайн';
+$lang['multiwan_primary'] = 'анхан шатны';
+$lang['multiwan_source_based_route'] = 'Эх сурвалж дээр суурилсан Route';
+$lang['multiwan_source_based_routes'] = 'Эх сурвалж дээр суурилсан Чиглүүлэлтүүд';
+$lang['multiwan_standby'] = 'зогсолт';
+$lang['multiwan_updating_status'] = 'Олон WAN статус мэдээллээ шинэчилж байна, энэ нь зөвхөн цөөн хэдэн секунд авч болно ...';
+$lang['multiwan_weight'] = 'Жин';
+$lang['multiwan_weight_invalid'] = 'Олон WAN жин нь хүчин төгөлдөр бус байна.';
+$lang['multiwan_weight_out_of_range'] = 'Олон WAN жин хүрээнээс гарч байна.';

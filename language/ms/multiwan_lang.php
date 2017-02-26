f52c11334953b705ad6e106eb2746334730faceb
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 16:46:06 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/ms/multiwan_lang.php
@@ -0,0 +1,22 @@
+<?php
+
+$lang['multiwan_app_description'] = 'Aplikasi Multi-WAN membolehkan anda untuk menyambung sistem anda untuk beberapa sambungan Internet. Multi-WAN menawarkan banyak faedah kepada persekitaran yang memerlukan sambungan yang boleh dipercayai, termasuk pengimbangan beban, pengasingan paket dan failover automatik.';
+$lang['multiwan_app_name'] = 'Multi-WAN';
+$lang['multiwan_backup'] = 'Backup';
+$lang['multiwan_destination_port_rule'] = 'Destinasi Port Peraturan';
+$lang['multiwan_destination_port_rules'] = 'Destinasi Port Peraturan';
+$lang['multiwan_in_use'] = 'Sedang digunakan';
+$lang['multiwan_mode'] = 'mod';
+$lang['multiwan_multiwan'] = 'Multi-WAN';
+$lang['multiwan_multiwan_status'] = 'Multi-WAN Status';
+$lang['multiwan_network_status'] = 'Status Network';
+$lang['multiwan_offline'] = 'offline';
+$lang['multiwan_online'] = 'talian';
+$lang['multiwan_primary'] = 'Primary';
+$lang['multiwan_source_based_route'] = 'berdasarkan sumber-Route';
+$lang['multiwan_source_based_routes'] = 'Laluan berdasarkan sumber-';
+$lang['multiwan_standby'] = 'siap sedia';
+$lang['multiwan_updating_status'] = 'Multi-WAN mengemaskini maklumat status, ini hanya akan mengambil beberapa saat ...';
+$lang['multiwan_weight'] = 'Berat';
+$lang['multiwan_weight_invalid'] = 'berat badan Multi-WAN tidak sah.';
+$lang['multiwan_weight_out_of_range'] = 'Multi-WAN berat badan adalah di luar liputan.';

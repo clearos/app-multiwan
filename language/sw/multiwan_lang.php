a9c01559d7f1872be085b7bfb48d804f3f1c9c11
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 16:48:40 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/sw/multiwan_lang.php
@@ -0,0 +1,22 @@
+<?php
+
+$lang['multiwan_app_description'] = 'programu Multi-WAN utapata kuungana mfumo wako kwa uhusiano nyingi Internet. Multi-WAN inatoa faida nyingi kwa mazingira kuhitaji uhusiano kuaminika, ikiwa ni pamoja na mzigo kusawazisha, pakiti ubaguzi na moja kwa moja failover.';
+$lang['multiwan_app_name'] = 'Multi-WAN';
+$lang['multiwan_backup'] = 'Backup';
+$lang['multiwan_destination_port_rule'] = 'Marudio Port Utawala';
+$lang['multiwan_destination_port_rules'] = 'Marudio Port Rules';
+$lang['multiwan_in_use'] = 'katika Matumizi';
+$lang['multiwan_mode'] = 'mode';
+$lang['multiwan_multiwan'] = 'Multi-WAN';
+$lang['multiwan_multiwan_status'] = 'Multi-WAN Hali';
+$lang['multiwan_network_status'] = 'Hali Network';
+$lang['multiwan_offline'] = 'offline';
+$lang['multiwan_online'] = 'online';
+$lang['multiwan_primary'] = 'Msingi';
+$lang['multiwan_source_based_route'] = 'Chanzo makao Route';
+$lang['multiwan_source_based_routes'] = 'Chanzo makao Routes';
+$lang['multiwan_standby'] = 'standby';
+$lang['multiwan_updating_status'] = 'Multi-WAN ni kuhuisha hadhi habari, hii itakuwa tu kuchukua sekunde chache ...';
+$lang['multiwan_weight'] = 'Uzito';
+$lang['multiwan_weight_invalid'] = 'Multi-WAN uzito ni batili.';
+$lang['multiwan_weight_out_of_range'] = 'Multi-WAN uzito ni nje ya mbalimbali.';

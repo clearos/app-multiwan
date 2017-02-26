d7add624a1a73dadc5c94ff22e80bf8b3e4eb9f4
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 16:39:57 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/az/multiwan_lang.php
@@ -0,0 +1,22 @@
+<?php
+
+$lang['multiwan_app_description'] = 'Multi-WAN app birdən çox internet bağlantısı üçün sistem qoşulmaq imkan verir. Multi-WAN load balancing, paket ayrı və avtomatik failover, o cümlədən etibarlı bağlantısı tələb mühit üçün bir çox fayda təklif edir.';
+$lang['multiwan_app_name'] = 'Multi-WAN';
+$lang['multiwan_backup'] = 'Backup';
+$lang['multiwan_destination_port_rule'] = 'Destination Port Rule';
+$lang['multiwan_destination_port_rules'] = 'Destination Port qaydaları';
+$lang['multiwan_in_use'] = 'İstifadə In';
+$lang['multiwan_mode'] = 'Mode';
+$lang['multiwan_multiwan'] = 'Multi-WAN';
+$lang['multiwan_multiwan_status'] = 'Multi-WAN Status';
+$lang['multiwan_network_status'] = 'Network Status';
+$lang['multiwan_offline'] = 'Offline';
+$lang['multiwan_online'] = 'Onlayn';
+$lang['multiwan_primary'] = 'əsas';
+$lang['multiwan_source_based_route'] = 'Source-based Route';
+$lang['multiwan_source_based_routes'] = 'Source-based Marşrutlar';
+$lang['multiwan_standby'] = 'Gözləmə';
+$lang['multiwan_updating_status'] = 'Multi-WAN statusu məlumat yenilənməsi, bu yalnız bir neçə saniyə olacaq ...';
+$lang['multiwan_weight'] = 'çəki';
+$lang['multiwan_weight_invalid'] = 'Multi-WAN çəki yanlışdır.';
+$lang['multiwan_weight_out_of_range'] = 'Multi-WAN çəki sıra deyil.';

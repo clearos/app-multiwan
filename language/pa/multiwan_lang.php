e693d16fc632e67f3a4b9c37c7d1b33ecac70793
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 16:46:46 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/pa/multiwan_lang.php
@@ -0,0 +1,22 @@
+<?php
+
+$lang['multiwan_app_description'] = 'ਮਲਟੀ-WAN ਐਪਲੀਕੇਸ਼ ਨੂੰ ਤੁਹਾਨੂੰ ਬਹੁਤੇ ਨੂੰ ਇੰਟਰਨੈੱਟ ਕੁਨੈਕਸ਼ਨ ਕਰਨ ਲਈ ਆਪਣੇ ਸਿਸਟਮ ਨਾਲ ਜੁੜਨ ਲਈ ਸਹਾਇਕ ਹੈ. ਮਲਟੀ-WAN ਲੋਡ ਬੈਲਸਿੰਗ, ਪੈਕੇਟ ਭੇਦਭਾਵ ਅਤੇ ਆਟੋਮੈਟਿਕ ਫੇਲਓਵਰ ਵੀ ਸ਼ਾਮਲ ਹੈ ਇੱਕ ਭਰੋਸੇਯੋਗ ਕੁਨੈਕਸ਼ਨ, ਲੋੜ ਵਾਤਾਵਰਣ ਲਈ ਬਹੁਤ ਸਾਰੇ ਲਾਭ ਦੀ ਪੇਸ਼ਕਸ਼ ਕਰਦਾ ਹੈ.';
+$lang['multiwan_app_name'] = 'ਮਲਟੀ-WAN';
+$lang['multiwan_backup'] = 'ਬੈਕਅੱਪ';
+$lang['multiwan_destination_port_rule'] = 'ਨਿਸ਼ਾਨਾ ਪੋਰ੍ਟ ਨਿਯਮ';
+$lang['multiwan_destination_port_rules'] = 'ਨਿਸ਼ਾਨਾ ਪੋਰ੍ਟ ਨਿਯਮ';
+$lang['multiwan_in_use'] = 'ਵਰਤੋ ਵਿੱਚ';
+$lang['multiwan_mode'] = 'ਮੋਡ';
+$lang['multiwan_multiwan'] = 'ਮਲਟੀ-WAN';
+$lang['multiwan_multiwan_status'] = 'ਮਲਟੀ-WAN ਹਾਲਤ';
+$lang['multiwan_network_status'] = 'ਨੈੱਟਵਰਕ ਹਾਲਤ';
+$lang['multiwan_offline'] = 'ਆਫ਼ਲਾਈਨ';
+$lang['multiwan_online'] = 'ਆਨਲਾਈਨ';
+$lang['multiwan_primary'] = 'ਪ੍ਰਾਇਮਰੀ';
+$lang['multiwan_source_based_route'] = 'ਸਰੋਤ-ਅਧਾਰਿਤ ਰੂਟ';
+$lang['multiwan_source_based_routes'] = 'ਸਰੋਤ-ਅਧਾਰਿਤ ਰੂਟ';
+$lang['multiwan_standby'] = 'ਨਾਲ ਖਲੋਣਾ';
+$lang['multiwan_updating_status'] = 'ਮਲਟੀ-WAN ਸਥਿਤੀ ਜਾਣਕਾਰੀ ਅੱਪਡੇਟ ਕਰ ਰਿਹਾ ਹੈ, ਇਸ ਨੂੰ ਸਿਰਫ ਕੁਝ ਕੁ ਸਕਿੰਟ ਲੈ ਜਾਵੇਗਾ ...';
+$lang['multiwan_weight'] = 'ਭਾਰ';
+$lang['multiwan_weight_invalid'] = 'ਮਲਟੀ-WAN ਭਾਰ ਗਲਤ ਹੈ.';
+$lang['multiwan_weight_out_of_range'] = 'ਮਲਟੀ-WAN ਭਾਰ ਸੀਮਾ ਦੇ ਬਾਹਰ ਹੈ.';

1b675e4d853a75756a3562e95713b01df18b060d
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 16:46:14 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/mt/multiwan_lang.php
@@ -0,0 +1,22 @@
+<?php
+
+$lang['multiwan_app_description'] = 'Il-app Multi-WAN jippermettilek li jgħaqqdu s-sistema tiegħek biex konnessjonijiet tal-Internet multipli. Multi-WAN joffri ħafna benefiċċji lill-ambjenti li jirrikjedu konnessjoni affidabbli, inkluż l-ibbilanċjar tat-tagħbija, is-segregazzjoni pakkett u failover awtomatiku.';
+$lang['multiwan_app_name'] = 'Multi-WAN';
+$lang['multiwan_backup'] = 'backup';
+$lang['multiwan_destination_port_rule'] = 'Destinazzjoni Regola Port';
+$lang['multiwan_destination_port_rules'] = 'Destinazzjoni Regoli Port';
+$lang['multiwan_in_use'] = 'fl-Użu';
+$lang['multiwan_mode'] = 'modalità';
+$lang['multiwan_multiwan'] = 'Multi-WAN';
+$lang['multiwan_multiwan_status'] = 'Multi-WAN Istatus';
+$lang['multiwan_network_status'] = 'Status netwerk';
+$lang['multiwan_offline'] = 'offline';
+$lang['multiwan_online'] = 'online';
+$lang['multiwan_primary'] = 'primarja';
+$lang['multiwan_source_based_route'] = 'bbażati fuq il-sors Rotta';
+$lang['multiwan_source_based_routes'] = 'Rotot bbażati fuq il-sors';
+$lang['multiwan_standby'] = 'standby';
+$lang['multiwan_updating_status'] = 'Multi-WAN Qed taġġorna informazzjoni dwar status, dan se jieħu biss ftit sekondi ...';
+$lang['multiwan_weight'] = 'piż';
+$lang['multiwan_weight_invalid'] = 'piż multi-WAN huwa invalidu.';
+$lang['multiwan_weight_out_of_range'] = 'Multi-WAN piż huwa barra mill-firxa.';

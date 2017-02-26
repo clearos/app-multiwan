da505bf8b1ad6455cf6c0eb9f7683b40934efacc
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 16:44:19 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/km/multiwan_lang.php
@@ -0,0 +1,22 @@
+<?php
+
+$lang['multiwan_app_description'] = 'កម្មវិធីពហុសមត្ថភាព WAN អនុញ្ញាតឱ្យអ្នកដើម្បីតភ្ជាប់ប្រព័ន្ធរបស់អ្នកដើម្បីតភ្ជាប់អ៊ីធឺណិច្រើន។ ពហុសមត្ថភាព WAN ផ្តល់នូវអត្ថប្រយោជន៍ជាច្រើនដល់បរិស្ថានតម្រូវឱ្យមានការតភ្ជាប់ដែលអាចជឿទុកចិត្ត, រួមបញ្ចូលទាំងតុល្យភាពផ្ទុកនិងបរាជ័យលើកញ្ចប់បែងចែកដោយស្វ័យប្រវត្តិ។';
+$lang['multiwan_app_name'] = 'ពហុសមត្ថភាព WAN';
+$lang['multiwan_backup'] = 'ការបម្រុងទុក';
+$lang['multiwan_destination_port_rule'] = 'វិធានកំពង់ផែគោលដៅ';
+$lang['multiwan_destination_port_rules'] = 'វិធានកំពង់ផែគោលដៅ';
+$lang['multiwan_in_use'] = 'កំពុង​ប្រើ';
+$lang['multiwan_mode'] = 'របៀប';
+$lang['multiwan_multiwan'] = 'ពហុសមត្ថភាព WAN';
+$lang['multiwan_multiwan_status'] = 'ស្ថានភាពពហុសមត្ថភាព WAN';
+$lang['multiwan_network_status'] = 'ស្ថានភាពបណ្តាញ';
+$lang['multiwan_offline'] = 'ក្រៅបណ្តាញ';
+$lang['multiwan_online'] = 'លើបណ្តាញ';
+$lang['multiwan_primary'] = 'បឋមសិក្សា';
+$lang['multiwan_source_based_route'] = 'ប្រភពដែលមានមូលដ្ឋានផ្លូវ';
+$lang['multiwan_source_based_routes'] = 'ផ្លូវមានមូលដ្ឋានប្រភព';
+$lang['multiwan_standby'] = 'ប្រចាំ​ការ';
+$lang['multiwan_updating_status'] = 'ពហុសមត្ថភាព WAN ត្រូវបានធ្វើឱ្យទាន់សម័យពស្ថានភាពនេះនឹងយកតែពីរបីវិនាទី ...';
+$lang['multiwan_weight'] = 'ទំងន់';
+$lang['multiwan_weight_invalid'] = 'ពហុសមត្ថភាព WAN ទម្ងន់គឺមិនត្រឹមត្រូវ។';
+$lang['multiwan_weight_out_of_range'] = 'ពហុសមត្ថភាព WAN ទម្ងន់គឺនៅក្រៅជួរ។';

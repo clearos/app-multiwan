70dc7eb478ca0ef50467875691228869904e903b
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 16:44:53 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/lo/multiwan_lang.php
@@ -0,0 +1,22 @@
+<?php
+
+$lang['multiwan_app_description'] = 'ການ app Multi-WAN ອະນຸຍາດໃຫ້ທ່ານເພື່ອເຊື່ອມຕໍ່ລະບົບຂອງທ່ານກັບການເຊື່ອມຕໍ່ອິນເຕີເນັດທີ່ຫຼາກຫຼາຍ. Multi-WAN ສະເຫນີຜົນປະໂຫຍດຫຼາຍກັບສະພາບແວດລ້ອມການກໍານົດການເຊື່ອມຕໍ່ທີ່ເຊື່ອຖືໄດ້, ລວມທັງການສົມດຸນພາ, ແຍກຊຸດແລະ failover ອັດຕະໂນມັດ.';
+$lang['multiwan_app_name'] = 'Multi-WAN';
+$lang['multiwan_backup'] = 'Backup';
+$lang['multiwan_destination_port_rule'] = 'ຈຸດຫມາຍປາຍທາງກົດລະບຽບ Port';
+$lang['multiwan_destination_port_rules'] = 'ຈຸດຫມາຍປາຍທາງກົດລະບຽບ Port';
+$lang['multiwan_in_use'] = 'ໃນການນໍາໃຊ້';
+$lang['multiwan_mode'] = 'ຮູບແບບການ';
+$lang['multiwan_multiwan'] = 'Multi-WAN';
+$lang['multiwan_multiwan_status'] = 'Multi-WAN ສະຖານະ';
+$lang['multiwan_network_status'] = 'ສະຖານະເຄືອຂ່າຍ';
+$lang['multiwan_offline'] = 'ອອຟໄລ';
+$lang['multiwan_online'] = 'ອອນໄລນ໌';
+$lang['multiwan_primary'] = 'ປະຖົມ';
+$lang['multiwan_source_based_route'] = 'ແຫຼ່ງຂໍ້ມູນທີ່ເສັ້ນທາງ';
+$lang['multiwan_source_based_routes'] = 'ເສັ້ນທາງແຫຼ່ງທີ່';
+$lang['multiwan_standby'] = 'Standby';
+$lang['multiwan_updating_status'] = 'Multi-WAN ແມ່ນການປັບປຸງຂໍ້ມູນສະຖານະພາບ, ນີ້ພຽງແຕ່ຈະໃຊ້ເວລາບໍ່ພໍເທົ່າໃດວິນາທີ ...';
+$lang['multiwan_weight'] = 'ນ້ໍາ';
+$lang['multiwan_weight_invalid'] = 'ນ້ໍາຫຼາຍ WAN ບໍ່ຖືກຕ້ອງ.';
+$lang['multiwan_weight_out_of_range'] = 'Multi-WAN ນ້ໍາອອກຈາກລະດັບ.';

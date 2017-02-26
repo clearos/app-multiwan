2bfdd1ba40c1530e7189a92626081dec60074427
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 16:43:31 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/ig/multiwan_lang.php
@@ -0,0 +1,22 @@
+<?php
+
+$lang['multiwan_app_description'] = 'The Multi-WAN ngwa-enye gị ohere jikọọ gị usoro na-multiple njikọ Ịntanetị. Multi-WAN awade ọtụtụ uru ka gburugburu nke chọrọ ka a pụrụ ịdabere na njikọ, gụnyere ibu adịzi, hà segregation na akpaka failover.';
+$lang['multiwan_app_name'] = 'Multi-WAN';
+$lang['multiwan_backup'] = 'ndabere';
+$lang['multiwan_destination_port_rule'] = 'Destination Port Ọma ahụ';
+$lang['multiwan_destination_port_rules'] = 'Destination Port Rules';
+$lang['multiwan_in_use'] = 'A Iji';
+$lang['multiwan_mode'] = 'mode';
+$lang['multiwan_multiwan'] = 'Multi-WAN';
+$lang['multiwan_multiwan_status'] = 'Multi-WAN Status';
+$lang['multiwan_network_status'] = 'Network Ọnọdụ';
+$lang['multiwan_offline'] = 'Na-anọghị n&#39;ịntanetị';
+$lang['multiwan_online'] = 'online';
+$lang['multiwan_primary'] = 'isi';
+$lang['multiwan_source_based_route'] = 'Isi Iyi dabeere na Nsoro';
+$lang['multiwan_source_based_routes'] = 'Isi Iyi dabeere na ụzọ gasị';
+$lang['multiwan_standby'] = 'Kwụrụ n&#39;ụsọ';
+$lang['multiwan_updating_status'] = 'Multi-WAN na-emelite ọnọdụ ọmụma, a ga na-ewere a ole na ole sekọnd ...';
+$lang['multiwan_weight'] = 'ibu ibu';
+$lang['multiwan_weight_invalid'] = 'Multi-WAN ibu bụ ghara ịdị irè.';
+$lang['multiwan_weight_out_of_range'] = 'Multi-WAN arọ bụ nke elu ugwu.';

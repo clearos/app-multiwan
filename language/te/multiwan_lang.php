e04657d24389b77a6431bce2db07a1e62c2c72ef
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 16:49:05 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/te/multiwan_lang.php
@@ -0,0 +1,22 @@
+<?php
+
+$lang['multiwan_app_description'] = 'బహుళ పాలిపోయిన అనువర్తనం మీరు బహుళ ఇంటర్నెట్ కనెక్షన్లు మీ సిస్టమ్ కనెక్ట్ అనుమతిస్తుంది. బహుళ పాలిపోయిన లోడ్ బాలెన్సింగ్, ప్యాకెట్ విభజించటానికి మరియు ఆటోమేటిక్ ఫెయిల్ ఓవర్ సహా ఒక నమ్మకమైన కనెక్షన్ అనవసరం, పరిసరాలలో అనేక ప్రయోజనాలను అందిస్తుంది.';
+$lang['multiwan_app_name'] = 'బహుళ పాలిపోయిన';
+$lang['multiwan_backup'] = 'బ్యాకప్';
+$lang['multiwan_destination_port_rule'] = 'గమ్యం పోర్ట్ రూల్';
+$lang['multiwan_destination_port_rules'] = 'గమ్యం పోర్ట్ రూల్స్';
+$lang['multiwan_in_use'] = 'వాడుకలో ఉన్నది';
+$lang['multiwan_mode'] = 'మోడ్';
+$lang['multiwan_multiwan'] = 'బహుళ పాలిపోయిన';
+$lang['multiwan_multiwan_status'] = 'బహుళ పాలిపోయిన స్థితి';
+$lang['multiwan_network_status'] = 'నెట్వర్క్ స్థితి';
+$lang['multiwan_offline'] = 'ఆఫ్లైన్';
+$lang['multiwan_online'] = 'ఆన్లైన్';
+$lang['multiwan_primary'] = 'ప్రాథమిక';
+$lang['multiwan_source_based_route'] = 'మూల ఆధారిత రూట్';
+$lang['multiwan_source_based_routes'] = 'మూల ఆధారిత మార్గాలు';
+$lang['multiwan_standby'] = 'స్టాండ్బై';
+$lang['multiwan_updating_status'] = 'బహుళ పాలిపోయిన స్థితి సమాచారాన్ని నవీకరిస్తున్నప్పుడు, ఈ కేవలం కొన్ని సెకన్లు పడుతుంది ...';
+$lang['multiwan_weight'] = 'బరువు';
+$lang['multiwan_weight_invalid'] = 'బహుళ పాలిపోయిన బరువు చెల్లదు.';
+$lang['multiwan_weight_out_of_range'] = 'బహుళ పాలిపోయిన బరువు పరిధి ముగిసింది.';

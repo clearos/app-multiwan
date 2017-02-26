554a87a391d62074c89b75d266228adaf09f95fb
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 16:40:06 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/eu/multiwan_lang.php
@@ -0,0 +1,22 @@
+<?php
+
+$lang['multiwan_app_description'] = 'Multi-WAN aplikazioarekin, zure sistema konektatu duzu Internet konexio bat baino gehiago da. Multi-WAN konexio fidagarria, karga orekatzeko, pakete-segregazioa eta failover automatikoarekin eskatzen ingurune onura asko eskaintzen.';
+$lang['multiwan_app_name'] = 'Multi-WAN';
+$lang['multiwan_backup'] = 'Backup';
+$lang['multiwan_destination_port_rule'] = 'Helmuga Port Araua';
+$lang['multiwan_destination_port_rules'] = 'Helmuga Port arauak';
+$lang['multiwan_in_use'] = 'erabilia';
+$lang['multiwan_mode'] = 'Modu';
+$lang['multiwan_multiwan'] = 'Multi-WAN';
+$lang['multiwan_multiwan_status'] = 'Multi-WAN Status';
+$lang['multiwan_network_status'] = 'Sarearen egoera';
+$lang['multiwan_offline'] = 'Offline';
+$lang['multiwan_online'] = 'Online';
+$lang['multiwan_primary'] = 'Lehen';
+$lang['multiwan_source_based_route'] = 'Iturburu-oinarritutako ibilbidea';
+$lang['multiwan_source_based_routes'] = 'Iturburu-oinarritutako Ibilbideak';
+$lang['multiwan_standby'] = 'Itxaron';
+$lang['multiwan_updating_status'] = 'Multi-WAN status informazioa eguneratzeko, hau bakarrik du segundo batzuk ...';
+$lang['multiwan_weight'] = 'Pisua';
+$lang['multiwan_weight_invalid'] = 'Multi-WAN pisua baliogabea da.';
+$lang['multiwan_weight_out_of_range'] = 'Multi-WAN pisua barrutitik kanpo.';

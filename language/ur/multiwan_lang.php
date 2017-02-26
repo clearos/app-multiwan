14785df478ab1c9d19168ea643f181902654ecaf
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 16:49:54 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/ur/multiwan_lang.php
@@ -0,0 +1,22 @@
+<?php
+
+$lang['multiwan_app_description'] = 'ملٹی وان اپلی کیشن آپ کو ایک سے زیادہ انٹرنیٹ کنکشن کے لئے اپنے نظام کو مربوط کرنے کے لئے کی اجازت دیتا. ملٹی وان لوڈ توازن، پیکٹ الگ تھلگ اور خودکار failover کے سمیت ایک قابل اعتماد کنکشن، کی ضرورت ہوتی ہے ماحول کے لئے بہت سے فوائد فراہم کرتا ہے.';
+$lang['multiwan_app_name'] = 'ملٹی وان';
+$lang['multiwan_backup'] = 'بیک اپ';
+$lang['multiwan_destination_port_rule'] = 'منزل پورٹ اصول';
+$lang['multiwan_destination_port_rules'] = 'منزل پورٹ قواعد';
+$lang['multiwan_in_use'] = 'استعمال میں';
+$lang['multiwan_mode'] = 'موڈ';
+$lang['multiwan_multiwan'] = 'ملٹی وان';
+$lang['multiwan_multiwan_status'] = 'ملٹی وان حیثیت';
+$lang['multiwan_network_status'] = 'نیٹ ورک کی حیثیت';
+$lang['multiwan_offline'] = 'آف لائن';
+$lang['multiwan_online'] = 'آن لائن';
+$lang['multiwan_primary'] = 'پرائمری';
+$lang['multiwan_source_based_route'] = 'ماخذ کی بنیاد پر روٹ';
+$lang['multiwan_source_based_routes'] = 'ماخذ کی بنیاد روٹس';
+$lang['multiwan_standby'] = 'تیار';
+$lang['multiwan_updating_status'] = 'ملٹی وان حیثیت کی معلومات کو اپ ڈیٹ کر رہا ہے، یہ صرف چند سیکنڈ لگیں گے ...';
+$lang['multiwan_weight'] = 'وزن';
+$lang['multiwan_weight_invalid'] = 'ملٹی وان وزن میں غلط ہے.';
+$lang['multiwan_weight_out_of_range'] = 'ملٹی وان وزن کی حد سے باہر ہے.';

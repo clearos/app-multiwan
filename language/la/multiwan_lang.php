f7535d31fecc9dbae5e4e384f745e728aab183a6
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 16:44:45 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/la/multiwan_lang.php
@@ -0,0 +1,22 @@
+<?php
+
+$lang['multiwan_app_description'] = 'Multi-Wan A app sino vos ad coniungere plures Penitus iunctio ut vestri ratio. Multi-Wan multis beneficia postulantes a fidelibus circa environments, etiam oneratis aequatisl, et fasciculum automatic failover segregationem.';
+$lang['multiwan_app_name'] = 'Multi-Wan';
+$lang['multiwan_backup'] = 'Tergum';
+$lang['multiwan_destination_port_rule'] = 'Destination Port regula';
+$lang['multiwan_destination_port_rules'] = 'Destination Port Rules';
+$lang['multiwan_in_use'] = 'In usum';
+$lang['multiwan_mode'] = 'Modus';
+$lang['multiwan_multiwan'] = 'Multi-Wan';
+$lang['multiwan_multiwan_status'] = 'Multi-Wan Status';
+$lang['multiwan_network_status'] = 'Status Network';
+$lang['multiwan_offline'] = 'offline';
+$lang['multiwan_online'] = 'Online';
+$lang['multiwan_primary'] = 'Primary';
+$lang['multiwan_source_based_route'] = 'Source-substructio Route';
+$lang['multiwan_source_based_routes'] = 'Itinera quae Source:';
+$lang['multiwan_standby'] = 'sto';
+$lang['multiwan_updating_status'] = 'Multi-Wan adaequationis status notitia est, hoc tantum pauci secundus ...';
+$lang['multiwan_weight'] = 'Weight';
+$lang['multiwan_weight_invalid'] = 'Multi-Wan pondus est irritum.';
+$lang['multiwan_weight_out_of_range'] = 'Multi-Wan pondus feriunt.';

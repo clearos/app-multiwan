628782bd62dbd2ded641df2f42f9da0cf537c7a6
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 16:42:51 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/ha/multiwan_lang.php
@@ -0,0 +1,22 @@
+<?php
+
+$lang['multiwan_app_description'] = 'The Multi-WAN app ka damar gama your tsarin mahara Internet sadarwa. Multi-WAN offers da yawa amfani ga yanayin bukata a dogara dangane, ciki har da load daidaita, fakiti fata da kuma m failover.';
+$lang['multiwan_app_name'] = 'Multi-WAN';
+$lang['multiwan_backup'] = 'Ajiyayyen';
+$lang['multiwan_destination_port_rule'] = 'Manufa Port Rule';
+$lang['multiwan_destination_port_rules'] = 'Manufa Port Dokokin';
+$lang['multiwan_in_use'] = 'a amfani';
+$lang['multiwan_mode'] = 'Mode';
+$lang['multiwan_multiwan'] = 'Multi-WAN';
+$lang['multiwan_multiwan_status'] = 'Multi-WAN Status';
+$lang['multiwan_network_status'] = 'Network Status';
+$lang['multiwan_offline'] = 'offline';
+$lang['multiwan_online'] = 'Online';
+$lang['multiwan_primary'] = 'primary';
+$lang['multiwan_source_based_route'] = 'Source tushen Route';
+$lang['multiwan_source_based_routes'] = 'Source tushen hanyoyin';
+$lang['multiwan_standby'] = 'Tsaya tukuna';
+$lang['multiwan_updating_status'] = 'Multi-WAN aka Ana ɗaukaka bayanin hali, wannan zai kawai yi a &#39;yan seconds ...';
+$lang['multiwan_weight'] = 'Weight';
+$lang['multiwan_weight_invalid'] = 'Multi-WAN nauyi ne ba daidai ba.';
+$lang['multiwan_weight_out_of_range'] = 'Multi-WAN nauyi ne daga fuska.';

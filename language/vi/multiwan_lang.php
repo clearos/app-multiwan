65d0142158db37753552cb768063db5558481a04
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 16:50:11 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/vi/multiwan_lang.php
@@ -0,0 +1,22 @@
+<?php
+
+$lang['multiwan_app_description'] = 'Các ứng dụng Multi-WAN cho phép bạn kết nối với hệ thống của bạn để nhiều kết nối Internet. Multi-WAN cung cấp nhiều lợi ích cho môi trường đòi hỏi một kết nối đáng tin cậy, bao gồm cân bằng tải, gói phân ly và chuyển đổi dự phòng tự động.';
+$lang['multiwan_app_name'] = 'Multi-WAN';
+$lang['multiwan_backup'] = 'Sao lưu';
+$lang['multiwan_destination_port_rule'] = 'Rule Destination Cảng';
+$lang['multiwan_destination_port_rules'] = 'Port Rules Điểm đến';
+$lang['multiwan_in_use'] = 'trong Sử dụng';
+$lang['multiwan_mode'] = 'Chế độ';
+$lang['multiwan_multiwan'] = 'Multi-WAN';
+$lang['multiwan_multiwan_status'] = 'Multi-WAN Status';
+$lang['multiwan_network_status'] = 'Tình trạng mạng';
+$lang['multiwan_offline'] = 'ẩn';
+$lang['multiwan_online'] = 'Trực tuyến';
+$lang['multiwan_primary'] = 'Sơ cấp';
+$lang['multiwan_source_based_route'] = 'Nguồn dựa trên Route';
+$lang['multiwan_source_based_routes'] = 'Các tuyến đường dựa trên nguồn';
+$lang['multiwan_standby'] = 'Đứng gần';
+$lang['multiwan_updating_status'] = 'Multi-WAN được cập nhật thông tin trạng thái, điều này sẽ chỉ mất vài giây ...';
+$lang['multiwan_weight'] = 'Cân nặng';
+$lang['multiwan_weight_invalid'] = 'Multi-WAN cân là không hợp lệ.';
+$lang['multiwan_weight_out_of_range'] = 'Multi-WAN cân là ra khỏi phạm vi.';

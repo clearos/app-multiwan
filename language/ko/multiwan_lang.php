f1f6bef5a24b1d488b4c88c024ec4d2782fce025
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 16:44:37 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/ko/multiwan_lang.php
@@ -0,0 +1,22 @@
+<?php
+
+$lang['multiwan_app_description'] = '멀티 WAN 애플리케이션은 여러 인터넷 연결로 시스템을 연결할 수 있습니다. 멀티 WAN로드 밸런싱, 패킷 분리 및 자동 장애 복구를 포함하여 신뢰할 수있는 연결을 필요로하는 환경에 많은 혜택을 제공합니다.';
+$lang['multiwan_app_name'] = '멀티 WAN';
+$lang['multiwan_backup'] = '지원';
+$lang['multiwan_destination_port_rule'] = '대상 포트 규칙';
+$lang['multiwan_destination_port_rules'] = '대상 포트 규칙';
+$lang['multiwan_in_use'] = '사용';
+$lang['multiwan_mode'] = '방법';
+$lang['multiwan_multiwan'] = '멀티 WAN';
+$lang['multiwan_multiwan_status'] = '멀티 WAN 상태';
+$lang['multiwan_network_status'] = '네트워크 상태';
+$lang['multiwan_offline'] = '오프라인';
+$lang['multiwan_online'] = '온라인으로';
+$lang['multiwan_primary'] = '행성';
+$lang['multiwan_source_based_route'] = '소스 기반의 라우팅';
+$lang['multiwan_source_based_routes'] = '소스 기반의 경로';
+$lang['multiwan_standby'] = '대기';
+$lang['multiwan_updating_status'] = '멀티 WAN 상태 정보를 갱신하고, 이것은 몇 초가 걸릴 것이다 ..';
+$lang['multiwan_weight'] = '무게';
+$lang['multiwan_weight_invalid'] = '멀티 WAN 무게가 잘못되었습니다.';
+$lang['multiwan_weight_out_of_range'] = '멀티 WAN 무게가 범위를 벗어났습니다.';

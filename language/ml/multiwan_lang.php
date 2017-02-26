9a34edd3cc2911353746218f931d26b34e86affb
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 16:45:42 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/ml/multiwan_lang.php
@@ -0,0 +1,22 @@
+<?php
+
+$lang['multiwan_app_description'] = 'മൾട്ടി-വാൻ അപ്ലിക്കേഷൻ നിങ്ങൾ ഒന്നിലധികം ഇന്റർനെറ്റ് കണക്ഷനുകൾ നിങ്ങളുടെ സിസ്റ്റം കണക്ട് അനുവദിക്കുന്നു. മൾട്ടി-വാൻ ലോഡ് ബാലൻസിങ്, പാക്കറ്റ് വേർതിരിവ് ഓട്ടോമാറ്റിക് പിശകുള്ള ഉൾപ്പെടെ ഒരു വിശ്വസനീയമായ കണക്ഷൻ, ആവശ്യമില്ലാതെ പരിതസ്ഥിതികൾ പല ആനുകൂല്യങ്ങളും.';
+$lang['multiwan_app_name'] = 'മൾട്ടി-വാൻ';
+$lang['multiwan_backup'] = 'ബാക്കപ്പ്';
+$lang['multiwan_destination_port_rule'] = 'ലക്ഷ്യസ്ഥാനം പോര്ട് റൂൾ';
+$lang['multiwan_destination_port_rules'] = 'ലക്ഷ്യസ്ഥാനം പോര്ട് നിയമങ്ങൾ';
+$lang['multiwan_in_use'] = 'ഉപയോഗത്തിലാണ്';
+$lang['multiwan_mode'] = 'മോഡ്';
+$lang['multiwan_multiwan'] = 'മൾട്ടി-വാൻ';
+$lang['multiwan_multiwan_status'] = 'മൾട്ടി-വാൻ അവസ്ഥ';
+$lang['multiwan_network_status'] = 'നെറ്റ്വർക്ക് അവസ്ഥ';
+$lang['multiwan_offline'] = 'ഓഫ്ലൈൻ';
+$lang['multiwan_online'] = 'ഓൺലൈൻ';
+$lang['multiwan_primary'] = 'പ്രാഥമിക';
+$lang['multiwan_source_based_route'] = 'അവലംബം അധിഷ്ഠിത റൂട്ട്';
+$lang['multiwan_source_based_routes'] = 'അവലംബം അധിഷ്ഠിത റൂട്ടുകൾ';
+$lang['multiwan_standby'] = 'സ്റ്റാൻഡ് ബൈ';
+$lang['multiwan_updating_status'] = 'മൾട്ടി-വാൻ ഈ കുറച്ചു സെക്കന്റുകൾ മാത്രമേ എടുക്കും, സ്റ്റാറ്റസ് വിവരം അപ്ഡേറ്റ് ...';
+$lang['multiwan_weight'] = 'ഭാരം';
+$lang['multiwan_weight_invalid'] = 'മൾട്ടി-വാൻ ഭാരം അസാധുവാണ്.';
+$lang['multiwan_weight_out_of_range'] = 'മൾട്ടി-വാൻ ഭാരം പരിധിക്ക് പുറത്താണ്.';

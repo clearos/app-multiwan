b58453e843c34b50175053a160ca09b64e4cb731
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 16:49:21 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/th/multiwan_lang.php
@@ -0,0 +1,22 @@
+<?php
+
+$lang['multiwan_app_description'] = 'แอปพลิเค Multi-WAN ช่วยให้คุณสามารถเชื่อมต่อระบบของคุณเพื่อเชื่อมต่ออินเตอร์เน็ตหลาย Multi-WAN มีประโยชน์มากมายกับสภาพแวดล้อมที่ต้องมีการเชื่อมต่อที่เชื่อถือรวมทั้งสมดุลภาระแยกแพ็คเก็ตและ failover อัตโนมัติ';
+$lang['multiwan_app_name'] = 'Multi-WAN';
+$lang['multiwan_backup'] = 'การสำรองข้อมูล';
+$lang['multiwan_destination_port_rule'] = 'ปลายทางกฎพอร์ต';
+$lang['multiwan_destination_port_rules'] = 'ปลายทางกฎของพอร์ต';
+$lang['multiwan_in_use'] = 'ในการใช้งาน';
+$lang['multiwan_mode'] = 'โหมด';
+$lang['multiwan_multiwan'] = 'Multi-WAN';
+$lang['multiwan_multiwan_status'] = 'Multi-WAN สถานะ';
+$lang['multiwan_network_status'] = 'สถานะเครือข่าย';
+$lang['multiwan_offline'] = 'ออฟไลน์';
+$lang['multiwan_online'] = 'ออนไลน์';
+$lang['multiwan_primary'] = 'ประถม';
+$lang['multiwan_source_based_route'] = 'มาตามเส้นทาง';
+$lang['multiwan_source_based_routes'] = 'เส้นทางที่มาตาม';
+$lang['multiwan_standby'] = 'รอ';
+$lang['multiwan_updating_status'] = 'Multi-WAN มีการปรับปรุงข้อมูลสถานะนี้จะใช้เวลาไม่กี่วินาที ...';
+$lang['multiwan_weight'] = 'น้ำหนัก';
+$lang['multiwan_weight_invalid'] = 'Multi-WAN น้ำหนักไม่ถูกต้อง';
+$lang['multiwan_weight_out_of_range'] = 'น้ำหนัก Multi-WAN อยู่นอกช่วง';

c18dfcf535b9348a091a7289bab73186d506e4ac
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 16:39:26 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/af/multiwan_lang.php
@@ -0,0 +1,22 @@
+<?php
+
+$lang['multiwan_app_description'] = 'Die Multi-WAN inligting kan jy jou stelsel aan te sluit op verskeie Internet verbindings. Multi-WAN bied baie voordele aan omgewings wat &#39;n betroubare verbinding, insluitend load balancing, pakkie segregasie en outomatiese fail.';
+$lang['multiwan_app_name'] = 'Multi-WAN';
+$lang['multiwan_backup'] = 'Ondersteuning';
+$lang['multiwan_destination_port_rule'] = 'Bestemming Port Reël';
+$lang['multiwan_destination_port_rules'] = 'Bestemming Port Reëls';
+$lang['multiwan_in_use'] = 'In gebruik';
+$lang['multiwan_mode'] = 'Wyse';
+$lang['multiwan_multiwan'] = 'Multi-WAN';
+$lang['multiwan_multiwan_status'] = 'Multi-WAN Status';
+$lang['multiwan_network_status'] = 'Network Status';
+$lang['multiwan_offline'] = 'op die regte pad';
+$lang['multiwan_online'] = 'Online';
+$lang['multiwan_primary'] = 'Laerskool';
+$lang['multiwan_source_based_route'] = 'Brongebaseerde Route';
+$lang['multiwan_source_based_routes'] = 'Brongebaseerde roetes';
+$lang['multiwan_standby'] = 'Staan by';
+$lang['multiwan_updating_status'] = 'Multi-WAN is die opdatering status inligting, sal dit slegs &#39;n paar sekondes ...';
+$lang['multiwan_weight'] = 'gewig';
+$lang['multiwan_weight_invalid'] = 'Multi-WAN gewig is ongeldig.';
+$lang['multiwan_weight_out_of_range'] = 'Multi-WAN gewig is buite bereik.';

7e54fc1db5a8c8169285d9a301e6386e97691004
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 16:40:29 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/bs/multiwan_lang.php
@@ -0,0 +1,22 @@
+<?php
+
+$lang['multiwan_app_description'] = 'Multi-WAN aplikacija omogućuje vam da spojite svoj sistem na više internet konekcije. Multi-WAN nudi mnoge prednosti okruženja zahtijevaju pouzdanu vezu, uključujući opterećenje balansiranje, paket segregacije i automatski failover.';
+$lang['multiwan_app_name'] = 'Multi-WAN';
+$lang['multiwan_backup'] = 'rezerva';
+$lang['multiwan_destination_port_rule'] = 'Destinacija Port pravilo';
+$lang['multiwan_destination_port_rules'] = 'Destinacija Port Pravila';
+$lang['multiwan_in_use'] = 'U upotrebi';
+$lang['multiwan_mode'] = 'način';
+$lang['multiwan_multiwan'] = 'Multi-WAN';
+$lang['multiwan_multiwan_status'] = 'Multi-WAN Status';
+$lang['multiwan_network_status'] = 'Status mreže';
+$lang['multiwan_offline'] = 'Offline';
+$lang['multiwan_online'] = 'online';
+$lang['multiwan_primary'] = 'osnovni';
+$lang['multiwan_source_based_route'] = 'Izvor-based Route';
+$lang['multiwan_source_based_routes'] = 'Izvor-based Rute';
+$lang['multiwan_standby'] = 'Pričekaj';
+$lang['multiwan_updating_status'] = 'Multi-WAN ažurira informacije o statusu, to će trajati samo nekoliko sekundi ...';
+$lang['multiwan_weight'] = 'težina';
+$lang['multiwan_weight_invalid'] = 'Multi-WAN težina je nevažeća.';
+$lang['multiwan_weight_out_of_range'] = 'Multi-WAN težina je izvan dometa.';

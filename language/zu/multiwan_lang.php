6d9165bec15296c3efe4acd189f6f76408b35183
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 16:50:44 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/zu/multiwan_lang.php
@@ -0,0 +1,22 @@
+<?php
+
+$lang['multiwan_app_description'] = 'Uhlelo lokusebenza Multi-WAN ikuvumela ukuba ukuxhuma ohlelweni lwakho amaningi uxhumano Inthanethi. Multi-WAN kunezinzuzo eziningi ukuba ezindaweni ezidinga ukuxhumana okuthembekile, kuhlanganise load sekulinganisa, iphakethe ukuhlukaniswa futhi failover othomathikhi.';
+$lang['multiwan_app_name'] = 'Multi-WAN';
+$lang['multiwan_backup'] = 'Backup';
+$lang['multiwan_destination_port_rule'] = 'Destination Port Rule';
+$lang['multiwan_destination_port_rules'] = 'Destination Port Rules';
+$lang['multiwan_in_use'] = 'In Sebenzisa';
+$lang['multiwan_mode'] = 'Mode';
+$lang['multiwan_multiwan'] = 'Multi-WAN';
+$lang['multiwan_multiwan_status'] = 'Multi-WAN Isimo';
+$lang['multiwan_network_status'] = 'Isimo Network';
+$lang['multiwan_offline'] = 'Okungaxhunyiwe ku-inthanethi';
+$lang['multiwan_online'] = 'Online';
+$lang['multiwan_primary'] = 'Primary';
+$lang['multiwan_source_based_route'] = 'Source-based Route';
+$lang['multiwan_source_based_routes'] = 'Imizila Source-based';
+$lang['multiwan_standby'] = 'Hlala ULINDILE';
+$lang['multiwan_updating_status'] = 'Multi-WAN is ubuyekeza imininingwane isimo, lokhu kungathatha imizuzwana embalwa ...';
+$lang['multiwan_weight'] = 'isisindo';
+$lang['multiwan_weight_invalid'] = 'Multi-WAN isisindo ayivumelekile.';
+$lang['multiwan_weight_out_of_range'] = 'Multi-WAN isisindo lingaphandle.';

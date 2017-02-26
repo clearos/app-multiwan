3c8b7f3a75ae214fc42e7f56d4ac1f63aba24c60
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 16:40:45 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/my/multiwan_lang.php
@@ -0,0 +1,22 @@
+<?php
+
+$lang['multiwan_app_description'] = 'အဆိုပါ Multi-WAN app ကိုသင်မျိုးစုံကအင်တာနက်ဆက်သွယ်မှုကိုမှသင့် system ကိုချိတ်ဆက်ရန်ခွင့်ပြုသည်။ multi-WAN ဝန် balancing, packet ကိုလူမျိုးရေးခွဲခြားမှုနဲ့ automatic failover အပါအဝင်တစ်ဦးယုံကြည်စိတ်ချရသောဆက်သွယ်မှု, လိုအပ်သောပတ်ဝန်းကျင်မှာအများကြီးအကျိုးခံစားခွင့်ပေးထားပါတယ်။';
+$lang['multiwan_app_name'] = 'multi-WAN';
+$lang['multiwan_backup'] = 'Backup ကို';
+$lang['multiwan_destination_port_rule'] = 'destination ဆိပ်ကမ်းနည်းဥပဒေ';
+$lang['multiwan_destination_port_rules'] = 'destination ဆိပ်ကမ်းစည်းကမ်းများ';
+$lang['multiwan_in_use'] = 'အသုံးပြုမှုအတွက်';
+$lang['multiwan_mode'] = 'mode ကို';
+$lang['multiwan_multiwan'] = 'multi-WAN';
+$lang['multiwan_multiwan_status'] = 'multi-WAN အခြေအနေ';
+$lang['multiwan_network_status'] = 'Network ကိုအဆင့်အတန်း';
+$lang['multiwan_offline'] = 'အော့ဖ်လိုင်း';
+$lang['multiwan_online'] = 'အွန်လိုင်း';
+$lang['multiwan_primary'] = 'မူလတန်း';
+$lang['multiwan_source_based_route'] = 'source-based လမ်းကြောင်း';
+$lang['multiwan_source_based_routes'] = 'source-based လမ်းကြောင်းများ';
+$lang['multiwan_standby'] = 'အရန်သင့်လုပ်သည်';
+$lang['multiwan_updating_status'] = 'multi-WAN ဒီသာစက္ကန့်အနည်းငယ်ကြာမြင့်မည်, အဆင့်အတန်းသတင်းအချက်အလက်ကို update ဖြစ်ပါတယ် ...';
+$lang['multiwan_weight'] = 'အလေးချိန်';
+$lang['multiwan_weight_invalid'] = 'multi-WAN အလေးချိန်မမှန်ကန်ဘူး။';
+$lang['multiwan_weight_out_of_range'] = 'multi-WAN အလေးချိန်အကွာအဝေးထဲကဖြစ်ပါတယ်။';

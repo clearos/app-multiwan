c9eb913d2c8d09e1b702e2f59c6c9a71f5c76cce
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 16:42:35 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/gu/multiwan_lang.php
@@ -0,0 +1,22 @@
+<?php
+
+$lang['multiwan_app_description'] = 'મલ્ટી નિસ્તેજ એપ્લિકેશન તમે બહુવિધ ઈન્ટરનેટ જોડાણો માટે તમારી સિસ્ટમ સાથે જોડાવા માટે પરવાનગી આપે છે. મલ્ટી નિસ્તેજ લોડ સંતુલન, પેકેટ અલગતા અને આપોઆપ ફેઈલઓવર સહિત વિશ્વસનીય જોડાણ, જરૂરી વાતાવરણ માટે ઘણા લાભો આપે છે.';
+$lang['multiwan_app_name'] = 'મલ્ટી નિસ્તેજ';
+$lang['multiwan_backup'] = 'બેકઅપ';
+$lang['multiwan_destination_port_rule'] = 'ગંતવ્ય બંદર નિયમ';
+$lang['multiwan_destination_port_rules'] = 'ગંતવ્ય બંદર નિયમો';
+$lang['multiwan_in_use'] = 'વપર઼ાશમાં';
+$lang['multiwan_mode'] = 'સ્થિતિ';
+$lang['multiwan_multiwan'] = 'મલ્ટી નિસ્તેજ';
+$lang['multiwan_multiwan_status'] = 'મલ્ટી નિસ્તેજ સ્થિતિ';
+$lang['multiwan_network_status'] = 'નેટવર્ક સ્થિતિ';
+$lang['multiwan_offline'] = 'ઑફલાઇન';
+$lang['multiwan_online'] = 'ઓનલાઇન';
+$lang['multiwan_primary'] = 'પ્રાથમિક';
+$lang['multiwan_source_based_route'] = 'સોર્સ આધારિત રૂટ';
+$lang['multiwan_source_based_routes'] = 'સોર્સ આધારિત રાઉટ';
+$lang['multiwan_standby'] = 'સ્ટેન્ડબાય';
+$lang['multiwan_updating_status'] = 'મલ્ટી નિસ્તેજ સ્થિતિ માહિતી અપડેટ થયેલ છે, આ માત્ર થોડી સેકન્ડોમાં લેશે ...';
+$lang['multiwan_weight'] = 'વજન';
+$lang['multiwan_weight_invalid'] = 'મલ્ટી નિસ્તેજ વજન અમાન્ય છે.';
+$lang['multiwan_weight_out_of_range'] = 'મલ્ટી નિસ્તેજ વજન શ્રેણી બહાર છે.';

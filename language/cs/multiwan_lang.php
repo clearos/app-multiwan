c14dd54a4fc519bf403814584012ac63623937c5
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 16:41:24 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/cs/multiwan_lang.php
@@ -0,0 +1,22 @@
+<?php
+
+$lang['multiwan_app_description'] = 'Aplikace Multi-WAN umožňuje připojit počítač k více připojení k Internetu. Multi-WAN nabízí mnoho výhod prostředích vyžadujících spolehlivé spojení, včetně vyrovnávání zatížení, paketů segregace a automatického odstraňování poruch.';
+$lang['multiwan_app_name'] = 'Multi-WAN';
+$lang['multiwan_backup'] = 'Zálohování';
+$lang['multiwan_destination_port_rule'] = 'Destination Port Pravidlo';
+$lang['multiwan_destination_port_rules'] = 'Cílový port Pravidla';
+$lang['multiwan_in_use'] = 'Při použití';
+$lang['multiwan_mode'] = 'režim';
+$lang['multiwan_multiwan'] = 'Multi-WAN';
+$lang['multiwan_multiwan_status'] = 'Multi-WAN Status';
+$lang['multiwan_network_status'] = 'Stav sítě';
+$lang['multiwan_offline'] = 'Offline';
+$lang['multiwan_online'] = 'Online';
+$lang['multiwan_primary'] = 'Hlavní';
+$lang['multiwan_source_based_route'] = 'Zdroj založený na trase';
+$lang['multiwan_source_based_routes'] = 'Zdroj založený na Trasy';
+$lang['multiwan_standby'] = 'Pohotovostní';
+$lang['multiwan_updating_status'] = 'Multi-WAN aktualizuje informace o stavu, bude to trvat jen pár vteřin ...';
+$lang['multiwan_weight'] = 'Hmotnost';
+$lang['multiwan_weight_invalid'] = 'Multi-WAN hmotnost je neplatná.';
+$lang['multiwan_weight_out_of_range'] = 'Multi-WAN hmotnost je mimo rozsah.';

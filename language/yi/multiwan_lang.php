539955e0b2ab9501dc23eefa3ef17a078116b0df
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 16:50:27 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/yi/multiwan_lang.php
@@ -0,0 +1,22 @@
+<?php
+
+$lang['multiwan_app_description'] = 'די מאַלטי-וואַן אַפּ אַלאַוז איר צו פאַרבינדן אייער סיסטעם צו קייפל אינטערנעץ קאַנעקשאַנז. מולטי-וואַן Offers פילע Benefits צו ינווייראַנמאַנץ ריקוויירינג אַ פאַרלאָזלעך קשר, כולל מאַסע באַלאַנסינג, פּאַקאַט סעגרעגאַציע און אָטאַמאַטיק פאַילאָווער.';
+$lang['multiwan_app_name'] = 'מולטי-וואַן';
+$lang['multiwan_backup'] = 'באַקקופּ';
+$lang['multiwan_destination_port_rule'] = 'דעסטינאַטיאָן פּאָרט הערשן';
+$lang['multiwan_destination_port_rules'] = 'דעסטינאַטיאָן פּאָרט רולעס';
+$lang['multiwan_in_use'] = 'אין נוצן';
+$lang['multiwan_mode'] = 'מאָדע';
+$lang['multiwan_multiwan'] = 'מולטי-וואַן';
+$lang['multiwan_multiwan_status'] = 'מולטי-וואַן סטאטוס';
+$lang['multiwan_network_status'] = 'נעץ סטאַטוס';
+$lang['multiwan_offline'] = 'offline';
+$lang['multiwan_online'] = 'אָנליין';
+$lang['multiwan_primary'] = 'ערשטיק';
+$lang['multiwan_source_based_route'] = 'מקור-באזירט רוט';
+$lang['multiwan_source_based_routes'] = 'מקור-באזירט ראָוטעס';
+$lang['multiwan_standby'] = 'סטאַנדבי';
+$lang['multiwan_updating_status'] = 'מולטי-וואַן איז אַפּדייטינג סטאַטוס אינפֿאָרמאַציע, דעם וועט נאָר נעמען אַ ביסל סעקונדעס ...';
+$lang['multiwan_weight'] = 'וואָג';
+$lang['multiwan_weight_invalid'] = 'מולטי-וואַן וואָג איז פאַרקריפּלט.';
+$lang['multiwan_weight_out_of_range'] = 'מולטי-וואַן וואָג איז אויס פון קייט.';

566661a05bb36c301f71940d0a50474646b5d7f7
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 16:44:27 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/kn/multiwan_lang.php
@@ -0,0 +1,22 @@
+<?php
+
+$lang['multiwan_app_description'] = 'ಬಹು ವಾನ್ ಅಪ್ಲಿಕೇಶನ್ ನೀವು ಅನೇಕ ಇಂಟರ್ನೆಟ್ ಸಂಪರ್ಕಗಳನ್ನು ನಿಮ್ಮ ವ್ಯವಸ್ಥೆಯ ಸಂಪರ್ಕ ಅನುಮತಿಸುತ್ತದೆ. ಬಹು ವಾನ್ ಹೊರೆ ಸಮತೋಲನೆ, ಪ್ಯಾಕೆಟ್ ಪ್ರತ್ಯೇಕತೆ ಮತ್ತು ಸ್ವಯಂಚಾಲಿತ ವಿಫಲಗೊಳ್ಳುವ ಸೇರಿದಂತೆ ವಿಶ್ವಾಸಾರ್ಹ ಸಂಪರ್ಕವನ್ನು ಅವಶ್ಯಕತೆ ಪರಿಸರದಲ್ಲಿ ಅನೇಕ ಅನುಕೂಲಗಳನ್ನು.';
+$lang['multiwan_app_name'] = 'ಬಹು ವಾನ್';
+$lang['multiwan_backup'] = 'ಬ್ಯಾಕಪ್';
+$lang['multiwan_destination_port_rule'] = 'ಗಮ್ಯಸ್ಥಾನ ಪೋರ್ಟ್ ರೂಲ್';
+$lang['multiwan_destination_port_rules'] = 'ಗಮ್ಯಸ್ಥಾನ ಪೋರ್ಟ್ ನಿಯಮಗಳು';
+$lang['multiwan_in_use'] = 'ಬಳಕೆಯಲ್ಲಿ';
+$lang['multiwan_mode'] = 'ಮೋಡ್';
+$lang['multiwan_multiwan'] = 'ಬಹು ವಾನ್';
+$lang['multiwan_multiwan_status'] = 'ಬಹು ವಾನ್ ಸ್ಥಿತಿ';
+$lang['multiwan_network_status'] = 'ನೆಟ್ವರ್ಕ್ ಸ್ಥಿತಿ';
+$lang['multiwan_offline'] = 'ಆಫ್ಲೈನ್';
+$lang['multiwan_online'] = 'ಆನ್ಲೈನ್';
+$lang['multiwan_primary'] = 'ಪ್ರಾಥಮಿಕ';
+$lang['multiwan_source_based_route'] = 'ಮೂಲ ಆಧಾರಿತ ಮಾರ್ಗ';
+$lang['multiwan_source_based_routes'] = 'ಮೂಲ ಆಧಾರಿತ ಮಾರ್ಗಗಳು';
+$lang['multiwan_standby'] = 'ಸ್ಟ್ಯಾಂಡ್ಬೈ';
+$lang['multiwan_updating_status'] = 'ಬಹು ವಾನ್ ಸ್ಥಿತಿ ಮಾಹಿತಿ ನವೀಕರಿಸುವುದಿಲ್ಲ, ಈ ಕೆಲವೇ ಸೆಕೆಂಡುಗಳ ತೆಗೆದುಕೊಳ್ಳುತ್ತದೆ ...';
+$lang['multiwan_weight'] = 'ತೂಕ';
+$lang['multiwan_weight_invalid'] = 'ಬಹು ವಾನ್ ತೂಕ ಅಮಾನ್ಯವಾಗಿದೆ.';
+$lang['multiwan_weight_out_of_range'] = 'ಬಹು ವಾನ್ ತೂಕ ಶ್ರೇಣಿಯ ಔಟ್.';

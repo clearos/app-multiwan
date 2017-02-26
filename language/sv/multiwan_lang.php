4e24948a0dec151139a2f28736edb5034f52bbed
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 16:48:48 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/sv/multiwan_lang.php
@@ -0,0 +1,22 @@
+<?php
+
+$lang['multiwan_app_description'] = 'Multi-WAN-appen gör att du kan ansluta datorn till flera Internetanslutningar. Multi-WAN erbjuder många fördelar för miljöer som kräver en tillförlitlig anslutning, inklusive lastbalansering, paket segregation och automatisk failover.';
+$lang['multiwan_app_name'] = 'Multi-WAN';
+$lang['multiwan_backup'] = 'Säkerhetskopiering';
+$lang['multiwan_destination_port_rule'] = 'Destination Port Rule';
+$lang['multiwan_destination_port_rules'] = 'Målport Regler';
+$lang['multiwan_in_use'] = 'I användning';
+$lang['multiwan_mode'] = 'Läge';
+$lang['multiwan_multiwan'] = 'Multi-WAN';
+$lang['multiwan_multiwan_status'] = 'Multi-WAN Status';
+$lang['multiwan_network_status'] = 'nätverks~~POS=TRUNC';
+$lang['multiwan_offline'] = 'Off-line';
+$lang['multiwan_online'] = 'Uppkopplad';
+$lang['multiwan_primary'] = 'Primär';
+$lang['multiwan_source_based_route'] = 'Källa baserade Route';
+$lang['multiwan_source_based_routes'] = 'Källa baserade Rutter';
+$lang['multiwan_standby'] = 'Står fast vid';
+$lang['multiwan_updating_status'] = 'Multi-WAN uppdaterar statusinformation, kommer det bara tar några sekunder ...';
+$lang['multiwan_weight'] = 'Vikt';
+$lang['multiwan_weight_invalid'] = 'Multi-WAN vikt är ogiltig.';
+$lang['multiwan_weight_out_of_range'] = 'Multi-WAN vikt är utom räckhåll.';

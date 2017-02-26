58c4093529f5b80bec11cf4b6592c2b93f3033fd
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 16:42:11 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/gl/multiwan_lang.php
@@ -0,0 +1,22 @@
+<?php
+
+$lang['multiwan_app_description'] = 'A aplicación Multi-WAN permite que Póñase o sistema para múltiples conexións de Internet. Múltiples WAN ofrece moitos beneficios para ambientes que requiren unha conexión fiable, incluíndo o balance de carga, a segregación de paquetes e failover automático.';
+$lang['multiwan_app_name'] = 'Múltiples WAN';
+$lang['multiwan_backup'] = 'backup';
+$lang['multiwan_destination_port_rule'] = 'Regra porto de destino';
+$lang['multiwan_destination_port_rules'] = 'Normas porto de destino';
+$lang['multiwan_in_use'] = 'en uso';
+$lang['multiwan_mode'] = 'modo';
+$lang['multiwan_multiwan'] = 'Múltiples WAN';
+$lang['multiwan_multiwan_status'] = 'Multi WAN-Estado';
+$lang['multiwan_network_status'] = 'estado da rede';
+$lang['multiwan_offline'] = 'offline';
+$lang['multiwan_online'] = 'en liña';
+$lang['multiwan_primary'] = 'fondo';
+$lang['multiwan_source_based_route'] = '-Source Baseada Route';
+$lang['multiwan_source_based_routes'] = 'Rutas baseados nas fontes';
+$lang['multiwan_standby'] = 'Estado de repouso';
+$lang['multiwan_updating_status'] = 'Múltiples WAN está actualizando a información de estado, iso só vai levar uns segundos ...';
+$lang['multiwan_weight'] = 'peso';
+$lang['multiwan_weight_invalid'] = 'peso Múltiples WAN é válido.';
+$lang['multiwan_weight_out_of_range'] = 'peso Múltiples WAN está fóra do intervalo.';

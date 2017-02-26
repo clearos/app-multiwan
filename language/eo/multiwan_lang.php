7e5328f81db669b48e73355d644c124b7c680d13
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 16:41:39 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/eo/multiwan_lang.php
@@ -0,0 +1,22 @@
+<?php
+
+$lang['multiwan_app_description'] = 'La Multi-WAN app permesas vin konekti vian sistemon por multnombraj Interreto rilatoj. Multi-WAN proponas multajn avantaĝojn al medioj postulanta fidinda ligo, inkluzive de ŝarĝo balancadon, paketon apartigo kaj aŭtomata Failover.';
+$lang['multiwan_app_name'] = 'Multi-WAN';
+$lang['multiwan_backup'] = 'apogilo';
+$lang['multiwan_destination_port_rule'] = 'Destino Haveno Regulo';
+$lang['multiwan_destination_port_rules'] = 'Destino Haveno Reguloj';
+$lang['multiwan_in_use'] = 'en Uzo';
+$lang['multiwan_mode'] = 'mode';
+$lang['multiwan_multiwan'] = 'Multi-WAN';
+$lang['multiwan_multiwan_status'] = 'Multi-WAN Statuso';
+$lang['multiwan_network_status'] = 'reto Statuso';
+$lang['multiwan_offline'] = 'offline';
+$lang['multiwan_online'] = 'Surreta';
+$lang['multiwan_primary'] = 'primaraj';
+$lang['multiwan_source_based_route'] = 'Fonto-bazita Itinero';
+$lang['multiwan_source_based_routes'] = 'Fonto-bazita Itineroj';
+$lang['multiwan_standby'] = 'standby';
+$lang['multiwan_updating_status'] = 'Multi-WAN estas ĝisdatiganta statinformoj, ĉi nur prenas kelkajn sekundojn ...';
+$lang['multiwan_weight'] = 'pezo';
+$lang['multiwan_weight_invalid'] = 'Multi-WAN pezo estas nevalida.';
+$lang['multiwan_weight_out_of_range'] = 'Multi-WAN pezo estas ekster atingo.';

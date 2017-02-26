1d2d3300a3ef52c4b6289c3c7b230c24d54cd7f8
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 16:39:42 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/ar/multiwan_lang.php
@@ -0,0 +1,22 @@
+<?php
+
+$lang['multiwan_app_description'] = 'التطبيق متعدد WAN يسمح لك لربط النظام الخاص بك إلى وصلات الانترنت متعددة. تقدم متعدد WAN العديد من الفوائد لبيئات التي تتطلب اتصال موثوق بها، بما في ذلك الموازنة، والفصل بين الحزم والفشل التلقائي.';
+$lang['multiwan_app_name'] = 'متعددة WAN';
+$lang['multiwan_backup'] = 'دعم';
+$lang['multiwan_destination_port_rule'] = 'ميناء الوجهة القاعدة';
+$lang['multiwan_destination_port_rules'] = 'قواعد المنفذ الوجهة';
+$lang['multiwan_in_use'] = 'في الاستخدام';
+$lang['multiwan_mode'] = 'الوضع';
+$lang['multiwan_multiwan'] = 'متعددة WAN';
+$lang['multiwan_multiwan_status'] = 'متعددة WAN الحالة';
+$lang['multiwan_network_status'] = 'حالة الشبكة';
+$lang['multiwan_offline'] = 'غير متصل على الانترنت';
+$lang['multiwan_online'] = 'عبر الانترنت';
+$lang['multiwan_primary'] = 'ابتدائي';
+$lang['multiwan_source_based_route'] = 'القائم على مصدر الطريق';
+$lang['multiwan_source_based_routes'] = 'طرق القائم على مصدر';
+$lang['multiwan_standby'] = 'تعليق';
+$lang['multiwan_updating_status'] = 'متعدد WAN يقوم بتحديث معلومات الحالة، وهذا سوف يستغرق سوى بضع ثوان ...';
+$lang['multiwan_weight'] = 'وزن';
+$lang['multiwan_weight_invalid'] = 'الوزن متعددة WAN غير صالح.';
+$lang['multiwan_weight_out_of_range'] = 'متعددة WAN الوزن هو خارج النطاق.';

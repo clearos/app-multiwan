2001bcdb60a736d6c05c05b679c8480aeef25d26
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 16:49:38 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/tr/multiwan_lang.php
@@ -0,0 +1,22 @@
+<?php
+
+$lang['multiwan_app_description'] = 'Multi-WAN uygulaması birden fazla Internet bağlantısı için sisteme bağlamak için. Multi-WAN yük dengelemesi, paket ayrıştırma ve otomatik hata da dahil olmak üzere güvenilir bir bağlantı gerektiren ortamlar için birçok faydalar sunmaktadır.';
+$lang['multiwan_app_name'] = 'Multi-WAN';
+$lang['multiwan_backup'] = 'yedek';
+$lang['multiwan_destination_port_rule'] = 'Hedef Port Kuralı';
+$lang['multiwan_destination_port_rules'] = 'Hedef Port Kuralları';
+$lang['multiwan_in_use'] = 'Kullanımda';
+$lang['multiwan_mode'] = 'kip';
+$lang['multiwan_multiwan'] = 'Multi-WAN';
+$lang['multiwan_multiwan_status'] = 'Multi-WAN Durumu';
+$lang['multiwan_network_status'] = 'ağ Durumu';
+$lang['multiwan_offline'] = 'Çevrimdışı';
+$lang['multiwan_online'] = 'İnternet üzerinden';
+$lang['multiwan_primary'] = 'Birincil';
+$lang['multiwan_source_based_route'] = 'Kaynak tabanlı Rota';
+$lang['multiwan_source_based_routes'] = 'Kaynak tabanlı Rotalar';
+$lang['multiwan_standby'] = 'Yanında olmak';
+$lang['multiwan_updating_status'] = 'Multi-WAN durum bilgilerini güncelliyor, bu sadece bir kaç saniye sürecektir ...';
+$lang['multiwan_weight'] = 'Ağırlık';
+$lang['multiwan_weight_invalid'] = 'Multi-WAN ağırlık geçersiz.';
+$lang['multiwan_weight_out_of_range'] = 'Multi-WAN ağırlık aralığının dışında.';

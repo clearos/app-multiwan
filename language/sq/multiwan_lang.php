c46b04f6a9f1d4beaee81ca40ac97ba74f87ff87
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 16:39:34 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/sq/multiwan_lang.php
@@ -0,0 +1,22 @@
+<?php
+
+$lang['multiwan_app_description'] = 'App Multi-WAN ju lejon të lidheni sistemin tuaj të lidhjeve të shumta të internetit. Multi-WAN ofron shumë përfitime për mjediset që kërkojnë një lidhje të besueshme, përfshirë balancimin e ngarkesës, segregacionit pako dhe failover automatike.';
+$lang['multiwan_app_name'] = 'Multi-WAN';
+$lang['multiwan_backup'] = 'rezervë';
+$lang['multiwan_destination_port_rule'] = 'Destinacioni Port Rregulla';
+$lang['multiwan_destination_port_rules'] = 'Destinacioni Rregullat Port';
+$lang['multiwan_in_use'] = 'Në përdorim';
+$lang['multiwan_mode'] = 'mënyrë';
+$lang['multiwan_multiwan'] = 'Multi-WAN';
+$lang['multiwan_multiwan_status'] = 'Multi-WAN Status';
+$lang['multiwan_network_status'] = 'Rrjeti Status';
+$lang['multiwan_offline'] = 'në linjë';
+$lang['multiwan_online'] = 'online';
+$lang['multiwan_primary'] = 'kryesor';
+$lang['multiwan_source_based_route'] = 'Source-bazuar Route';
+$lang['multiwan_source_based_routes'] = 'Rrugët burim me bazë';
+$lang['multiwan_standby'] = 'Standby';
+$lang['multiwan_updating_status'] = 'Multi-WAN është përditësimin e informacionit të statusit, kjo do të marrë vetëm disa sekonda ...';
+$lang['multiwan_weight'] = 'peshë';
+$lang['multiwan_weight_invalid'] = 'pesha Multi-WAN është e pavlefshme.';
+$lang['multiwan_weight_out_of_range'] = 'Multi-WAN peshë është jashtë rrezes.';

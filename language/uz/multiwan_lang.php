4d8621ef2aa78f8cf8b5ddc12f935b0ee63cb58e
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 16:50:03 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/uz/multiwan_lang.php
@@ -0,0 +1,22 @@
+<?php
+
+$lang['multiwan_app_description'] = 'Bir nechta WAN ilova, bir necha Internet aloqasi uchun tizim ulash imkonini beradi. Bir nechta WAN yuk dengeleme, paketli ajratish va avtomatik xato, shu jumladan, ishonchli ulanishini talab muhitlarga ko&#39;p foyda taklif etadi.';
+$lang['multiwan_app_name'] = 'Multi-WAN';
+$lang['multiwan_backup'] = 'zaxira';
+$lang['multiwan_destination_port_rule'] = 'Destination Port qoida';
+$lang['multiwan_destination_port_rules'] = 'Destination Port qoidalari';
+$lang['multiwan_in_use'] = 'Foydalanish In';
+$lang['multiwan_mode'] = 'usul';
+$lang['multiwan_multiwan'] = 'Multi-WAN';
+$lang['multiwan_multiwan_status'] = 'Multi-WAN Status';
+$lang['multiwan_network_status'] = 'tarmoq Status';
+$lang['multiwan_offline'] = 'Offline';
+$lang['multiwan_online'] = 'Online';
+$lang['multiwan_primary'] = 'boshlang&#39;ich';
+$lang['multiwan_source_based_route'] = 'Manba asoslangan Marshrut';
+$lang['multiwan_source_based_routes'] = 'Manba asoslangan Yo&#39;nalish';
+$lang['multiwan_standby'] = 'Kuting';
+$lang['multiwan_updating_status'] = 'Bir nechta WAN holati malumotlarni yangilash, bu faqat bir necha soniya davom etadi ...';
+$lang['multiwan_weight'] = 'vazn';
+$lang['multiwan_weight_invalid'] = 'Bir nechta WAN vazn yaroqsiz hisoblanadi.';
+$lang['multiwan_weight_out_of_range'] = 'Bir nechta WAN vazn intervalli tashqari.';

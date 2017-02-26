f7ab2d295920ed6a8003a360490f931543697b9a
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 16:40:21 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/bn/multiwan_lang.php
@@ -0,0 +1,22 @@
+<?php
+
+$lang['multiwan_app_description'] = 'মাল্টি অস্পষ্ট অ্যাপ্লিকেশন আপনি একাধিক ইন্টারনেট সংযোগ থেকে আপনার সিস্টেমের সাথে সংযোগ করতে সক্ষম হবেন. মাল্টি অস্পষ্ট ভারসাম্য প্যাকেট পৃথকীকরণ এবং স্বয়ংক্রিয় ফেলওভার সহ একটি নির্ভরযোগ্য সংযোগ, প্রয়োজন পরিবেশের অনেক সুবিধা উপলব্ধ করা হয়.';
+$lang['multiwan_app_name'] = 'মাল্টি অস্পষ্ট';
+$lang['multiwan_backup'] = 'ব্যাকআপ';
+$lang['multiwan_destination_port_rule'] = 'গন্তব্য পোর্ট রুল';
+$lang['multiwan_destination_port_rules'] = 'গন্তব্য পোর্ট বিধি';
+$lang['multiwan_in_use'] = 'ব্যাবহৃত হচ্ছে';
+$lang['multiwan_mode'] = 'মোড';
+$lang['multiwan_multiwan'] = 'মাল্টি অস্পষ্ট';
+$lang['multiwan_multiwan_status'] = 'মাল্টি অস্পষ্ট স্থিতি';
+$lang['multiwan_network_status'] = 'নেটওয়ার্ক স্থিতি';
+$lang['multiwan_offline'] = 'অফলাইন';
+$lang['multiwan_online'] = 'অনলাইন';
+$lang['multiwan_primary'] = 'প্রাথমিক';
+$lang['multiwan_source_based_route'] = 'উত্স ভিত্তিক রুট';
+$lang['multiwan_source_based_routes'] = 'উত্স ভিত্তিক রুট';
+$lang['multiwan_standby'] = 'অপেক্ষা করো';
+$lang['multiwan_updating_status'] = 'মাল্টি অস্পষ্ট অবস্থা তথ্য আপডেট করা হয়, এই মাত্র কয়েক সেকেন্ডের নিতে হবে ...';
+$lang['multiwan_weight'] = 'ওজন';
+$lang['multiwan_weight_invalid'] = 'মাল্টি অস্পষ্ট ওজন অবৈধ.';
+$lang['multiwan_weight_out_of_range'] = 'মাল্টি অস্পষ্ট ওজন সীমার বাইরে.';

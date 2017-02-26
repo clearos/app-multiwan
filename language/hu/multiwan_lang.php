d9df243ad0f2db28e9635c5e7e608fd07b4f1e54
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 16:43:07 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/hu/multiwan_lang.php
@@ -0,0 +1,22 @@
+<?php
+
+$lang['multiwan_app_description'] = 'A Multi-WAN app lehetővé teszi, hogy csatlakoztassa a rendszert több internetes kapcsolatot. Multi-WAN számos előnyt kínál a környezetbe, amely megbízható kapcsolatot, beleértve a terheléselosztás, csomag szegregáció és az automatikus átállást.';
+$lang['multiwan_app_name'] = 'Multi-WAN';
+$lang['multiwan_backup'] = 'biztonsági mentés';
+$lang['multiwan_destination_port_rule'] = 'Destination Port szabály';
+$lang['multiwan_destination_port_rules'] = 'Destination Port szabályok';
+$lang['multiwan_in_use'] = 'Használatban';
+$lang['multiwan_mode'] = 'Mód';
+$lang['multiwan_multiwan'] = 'Multi-WAN';
+$lang['multiwan_multiwan_status'] = 'Multi-WAN állapota';
+$lang['multiwan_network_status'] = 'A hálózat állapota';
+$lang['multiwan_offline'] = 'Offline';
+$lang['multiwan_online'] = 'Online';
+$lang['multiwan_primary'] = 'Elsődleges';
+$lang['multiwan_source_based_route'] = 'Forrás alapú útvonal';
+$lang['multiwan_source_based_routes'] = 'Forrás alapú Routes';
+$lang['multiwan_standby'] = 'Készenlétben lévő';
+$lang['multiwan_updating_status'] = 'Multi-WAN frissíti állapotáról, ez csak pár másodpercet vesz igénybe ...';
+$lang['multiwan_weight'] = 'Súly';
+$lang['multiwan_weight_invalid'] = 'Multi-WAN súlya érvénytelen.';
+$lang['multiwan_weight_out_of_range'] = 'Multi-WAN súlya a tartományon kívül van.';

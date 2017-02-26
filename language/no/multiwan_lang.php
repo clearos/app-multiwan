e2ae2f4032d93828700fded33f393f26084b0949
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 16:46:38 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/no/multiwan_lang.php
@@ -0,0 +1,22 @@
+<?php
+
+$lang['multiwan_app_description'] = 'Multi-WAN app lar deg koble systemet til flere Internett-tilkoblinger. Multi-WAN har mange fordeler for miljøer som krever en pålitelig tilkobling, inkludert lastbalansering, pakke segregering og automatisk failover.';
+$lang['multiwan_app_name'] = 'Multi-WAN';
+$lang['multiwan_backup'] = 'backup';
+$lang['multiwan_destination_port_rule'] = 'Destination Port Rule';
+$lang['multiwan_destination_port_rules'] = 'Destination Port Regler';
+$lang['multiwan_in_use'] = 'I bruk';
+$lang['multiwan_mode'] = 'Modus';
+$lang['multiwan_multiwan'] = 'Multi-WAN';
+$lang['multiwan_multiwan_status'] = 'Multi-WAN Status';
+$lang['multiwan_network_status'] = 'Nettverksstatus';
+$lang['multiwan_offline'] = 'offline';
+$lang['multiwan_online'] = 'på nett';
+$lang['multiwan_primary'] = 'primær';
+$lang['multiwan_source_based_route'] = 'Source-basert Route';
+$lang['multiwan_source_based_routes'] = 'Source-baserte ruter';
+$lang['multiwan_standby'] = 'Vent litt';
+$lang['multiwan_updating_status'] = 'Multi-WAN oppdaterer statusinformasjon, dette vil bare ta noen få sekunder ...';
+$lang['multiwan_weight'] = 'Vekt';
+$lang['multiwan_weight_invalid'] = 'Multi-WAN vekt er ugyldig.';
+$lang['multiwan_weight_out_of_range'] = 'Multi-WAN vekt er utenfor rekkevidde.';

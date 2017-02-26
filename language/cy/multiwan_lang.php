27feb21d3f36058f8a1c26421969fdb22d4d1670
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 16:50:19 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/cy/multiwan_lang.php
@@ -0,0 +1,22 @@
+<?php
+
+$lang['multiwan_app_description'] = 'Mae&#39;r app Aml-WAN yn eich galluogi i gysylltu eich system i cysylltiadau Rhyngrwyd lluosog. Aml-WAN cynnig llawer o fanteision i amgylcheddau sy&#39;n gofyn am gysylltiad dibynadwy, gan gynnwys cydbwyso llwyth, gwahanu paced a failover awtomatig.';
+$lang['multiwan_app_name'] = 'Aml-WAN';
+$lang['multiwan_backup'] = 'Backup';
+$lang['multiwan_destination_port_rule'] = 'Cyrchfan Rheol Port';
+$lang['multiwan_destination_port_rules'] = 'Rheolau Port Cyrchfan';
+$lang['multiwan_in_use'] = 'yn Defnyddio';
+$lang['multiwan_mode'] = 'modd';
+$lang['multiwan_multiwan'] = 'Aml-WAN';
+$lang['multiwan_multiwan_status'] = '-Aml WAN Statws';
+$lang['multiwan_network_status'] = 'Statws rhwydwaith';
+$lang['multiwan_offline'] = 'All-lein';
+$lang['multiwan_online'] = 'Ar-lein';
+$lang['multiwan_primary'] = 'cynradd';
+$lang['multiwan_source_based_route'] = 'seiliedig Ffynhonnell-Route';
+$lang['multiwan_source_based_routes'] = 'Llwybrau seiliedig Ffynhonnell-';
+$lang['multiwan_standby'] = 'standby';
+$lang['multiwan_updating_status'] = 'Aml-WAN yn diweddaru gwybodaeth statws, bydd hyn ond yn cymryd ychydig eiliadau ...';
+$lang['multiwan_weight'] = 'pwysau';
+$lang['multiwan_weight_invalid'] = 'Aml-WAN pwysau yn annilys.';
+$lang['multiwan_weight_out_of_range'] = 'Aml-WAN pwysau allan o ystod.';

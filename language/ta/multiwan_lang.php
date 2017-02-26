007e356b3b6afbf74e63f8dae2bac611e100e7c5
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 16:48:57 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/ta/multiwan_lang.php
@@ -0,0 +1,22 @@
+<?php
+
+$lang['multiwan_app_description'] = 'மல்டி WAN, பயன்பாட்டை நீங்கள் பல இணைய இணைப்புகளை உங்கள் கணினியில் இணைக்க அனுமதிக்கிறது. பல WAN, சுமை சமநிலை, பாக்கெட், பாகுபாடு மற்றும் தானியங்கி தோல்விக்கு உட்பட ஒரு நம்பகமான இணைப்பு, தேவைப்படும் சூழலில் பல நன்மைகளை வழங்குகிறது.';
+$lang['multiwan_app_name'] = 'பல தூரங்களில்';
+$lang['multiwan_backup'] = 'காப்பு';
+$lang['multiwan_destination_port_rule'] = 'இலக்கு போர்ட் விதி';
+$lang['multiwan_destination_port_rules'] = 'இலக்கு போர்ட் விதிகள்';
+$lang['multiwan_in_use'] = 'பயன்பாட்டில் உள்ளது';
+$lang['multiwan_mode'] = 'முறை';
+$lang['multiwan_multiwan'] = 'பல தூரங்களில்';
+$lang['multiwan_multiwan_status'] = 'பல தூரங்களில் நிலைமை';
+$lang['multiwan_network_status'] = 'பிணைய நிலை';
+$lang['multiwan_offline'] = 'ஆஃப்லைன்';
+$lang['multiwan_online'] = 'ஆன்லைன்';
+$lang['multiwan_primary'] = 'முதன்மை';
+$lang['multiwan_source_based_route'] = 'ஓப்பன் சோர்ஸ் அடிப்படையிலான வழி';
+$lang['multiwan_source_based_routes'] = 'ஓப்பன் சோர்ஸ் அடிப்படையிலான தடங்கல்';
+$lang['multiwan_standby'] = 'காத்திரு';
+$lang['multiwan_updating_status'] = 'பல தூரங்களில் நிலை தகவல் மேம்படுத்தும், இந்த ஒரு சில விநாடிகள் எடுக்கும் ...';
+$lang['multiwan_weight'] = 'எடை';
+$lang['multiwan_weight_invalid'] = 'பல WAN, எடை தவறானது.';
+$lang['multiwan_weight_out_of_range'] = 'பல WAN, எடை வரம்பில் இல்லை.';

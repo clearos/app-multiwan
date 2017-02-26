c331691fccc7fa5ebaf75c4a3f56ecefa1d35d07
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 16:49:30 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/tl/multiwan_lang.php
@@ -0,0 +1,22 @@
+<?php
+
+$lang['multiwan_app_description'] = 'Pinapayagan ka ng Multi-Wan app sa iyo upang kumonekta sa iyong system upang ang maramihang mga koneksyon sa Internet. Multi-Wan ay nagbibigay ng maraming mga benepisyo sa kapaligiran na nangangailangan ng isang maaasahang koneksyon, kabilang ang load balancing, packet paghihiwalay at awtomatikong failover.';
+$lang['multiwan_app_name'] = 'Multi-Wan';
+$lang['multiwan_backup'] = 'backup';
+$lang['multiwan_destination_port_rule'] = 'Destinasyon Port Rule';
+$lang['multiwan_destination_port_rules'] = 'Destinasyon Port Rules';
+$lang['multiwan_in_use'] = 'Sa paggamit';
+$lang['multiwan_mode'] = 'paraan';
+$lang['multiwan_multiwan'] = 'Multi-Wan';
+$lang['multiwan_multiwan_status'] = 'Multi-Wan Katayuan';
+$lang['multiwan_network_status'] = 'Katayuan network';
+$lang['multiwan_offline'] = 'offline';
+$lang['multiwan_online'] = 'online';
+$lang['multiwan_primary'] = 'pangunahin';
+$lang['multiwan_source_based_route'] = 'Source-based Route';
+$lang['multiwan_source_based_routes'] = 'Source-based Ruta';
+$lang['multiwan_standby'] = 'standby';
+$lang['multiwan_updating_status'] = 'Multi-Wan ay pag-update ng impormasyon sa katayuan, ito ay lamang tumagal ng ilang segundo ...';
+$lang['multiwan_weight'] = 'timbang';
+$lang['multiwan_weight_invalid'] = 'Multi-Wan timbang ay hindi wasto.';
+$lang['multiwan_weight_out_of_range'] = 'Multi-Wan timbang ay wala sa sakop.';

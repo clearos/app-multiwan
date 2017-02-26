bec6e34a4f80a88d300dc701a8f9106e70e27848
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 16:44:03 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/ka/multiwan_lang.php
@@ -0,0 +1,22 @@
+<?php
+
+$lang['multiwan_app_description'] = 'Multi-WAN app გაძლევთ საშუალებას თქვენი სისტემის მრავალჯერადი ინტერნეტ კავშირები. Multi-WAN სთავაზობს ბევრი სარგებლის გარემოში მოითხოვს საიმედო კავშირი, მათ შორის დატვირთვის დაბალანსება, პაკეტი სეგრეგაციისა და ავტომატური failover.';
+$lang['multiwan_app_name'] = 'Multi-WAN';
+$lang['multiwan_backup'] = 'Backup';
+$lang['multiwan_destination_port_rule'] = 'დანიშნულების პორტი Rule';
+$lang['multiwan_destination_port_rules'] = 'დანიშნულების პორტი წესები';
+$lang['multiwan_in_use'] = 'გამოყენების';
+$lang['multiwan_mode'] = 'რეჟიმი';
+$lang['multiwan_multiwan'] = 'Multi-WAN';
+$lang['multiwan_multiwan_status'] = 'Multi-WAN სტატუსი';
+$lang['multiwan_network_status'] = 'ქსელი სტატუსი';
+$lang['multiwan_offline'] = 'შემოსული არ არის';
+$lang['multiwan_online'] = 'Online';
+$lang['multiwan_primary'] = 'ძირითადი';
+$lang['multiwan_source_based_route'] = 'წყარო დაფუძნებული მარშრუტი';
+$lang['multiwan_source_based_routes'] = 'წყარო დაფუძნებული მარშრუტები';
+$lang['multiwan_standby'] = 'ლოდინის';
+$lang['multiwan_updating_status'] = 'Multi-WAN განახლებას სტატუსის შესახებ, ეს იქნება მხოლოდ რამდენიმე წამი ...';
+$lang['multiwan_weight'] = 'წონა';
+$lang['multiwan_weight_invalid'] = 'Multi-WAN წონა არასწორია.';
+$lang['multiwan_weight_out_of_range'] = 'Multi-WAN წონა გარეთ სპექტრი.';

f55c6033b0e08e17fe7a47f6aa126df980e99c65
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 16:43:23 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/ga/multiwan_lang.php
@@ -0,0 +1,22 @@
+<?php
+
+$lang['multiwan_app_description'] = 'Ceadaíonn an app Il-WAN tú do chóras chun ceangal le naisc Idirlín il. Il-WAN tairiscintí go leor buntáistí do timpeallachtaí a éilíonn nasc iontaofa, lena n-áirítear cothromú ualach, deighilt data agus failover uathoibríoch.';
+$lang['multiwan_app_name'] = 'Il-WAN';
+$lang['multiwan_backup'] = 'cúltaca';
+$lang['multiwan_destination_port_rule'] = 'Scríbe Port Riail';
+$lang['multiwan_destination_port_rules'] = 'Scríbe Rialacha Port';
+$lang['multiwan_in_use'] = 'i Úsáid';
+$lang['multiwan_mode'] = 'mód';
+$lang['multiwan_multiwan'] = 'Il-WAN';
+$lang['multiwan_multiwan_status'] = 'Il-WAN Stádas';
+$lang['multiwan_network_status'] = 'Stádas líonra';
+$lang['multiwan_offline'] = 'líne';
+$lang['multiwan_online'] = 'Líne';
+$lang['multiwan_primary'] = 'Bunscoileanna';
+$lang['multiwan_source_based_route'] = 'Foinse-bhunaithe Bealach';
+$lang['multiwan_source_based_routes'] = 'Bealaí Foinse-bhunaithe';
+$lang['multiwan_standby'] = 'fuireachas';
+$lang['multiwan_updating_status'] = 'Il-WAN nuashonrú faisnéis maidir le stádas, beidh sé seo a ghlacadh ach cúpla soicind ...';
+$lang['multiwan_weight'] = 'meáchan';
+$lang['multiwan_weight_invalid'] = 'Is Il-WAN meáchan neamhbhailí.';
+$lang['multiwan_weight_out_of_range'] = 'Is Il-WAN meáchan as raon.';

a8095b4f652de3c5becbbbd0e26ccc61d3dbd0dc
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 16:42:27 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/el/multiwan_lang.php
@@ -0,0 +1,22 @@
+<?php
+
+$lang['multiwan_app_description'] = 'Η εφαρμογή Multi-WAN σας επιτρέπει να συνδέσετε το σύστημά σας σε πολλαπλές συνδέσεις στο Internet. Multi-WAN προσφέρει πολλά πλεονεκτήματα σε περιβάλλοντα που απαιτούν μια αξιόπιστη σύνδεση, συμπεριλαμβανομένης της εξισορρόπησης φορτίου, το πακέτο διαχωρισμού και αυτόματο failover.';
+$lang['multiwan_app_name'] = 'Multi-WAN';
+$lang['multiwan_backup'] = 'Αντιγράφων ασφαλείας';
+$lang['multiwan_destination_port_rule'] = 'Destination Port Κανόνας';
+$lang['multiwan_destination_port_rules'] = 'Προορισμός Κανόνες Λιμάνι';
+$lang['multiwan_in_use'] = 'Σε χρήση';
+$lang['multiwan_mode'] = 'Τρόπος';
+$lang['multiwan_multiwan'] = 'Multi-WAN';
+$lang['multiwan_multiwan_status'] = 'Multi-WAN Κατάσταση';
+$lang['multiwan_network_status'] = 'Κατάσταση δικτύου';
+$lang['multiwan_offline'] = 'offline';
+$lang['multiwan_online'] = 'σε απευθείας σύνδεση';
+$lang['multiwan_primary'] = 'Πρωταρχικός';
+$lang['multiwan_source_based_route'] = 'Πηγή-based Διαδρομή';
+$lang['multiwan_source_based_routes'] = 'Διαδρομές της ρύπανσης στην πηγή';
+$lang['multiwan_standby'] = 'Αναμονή';
+$lang['multiwan_updating_status'] = 'Multi-WAN είναι ενημέρωση πληροφορίες για την κατάσταση, αυτό θα πάρει μόνο λίγα δευτερόλεπτα ...';
+$lang['multiwan_weight'] = 'Βάρος';
+$lang['multiwan_weight_invalid'] = 'βάρος Multi-WAN δεν είναι έγκυρο.';
+$lang['multiwan_weight_out_of_range'] = 'Multi-WAN βάρος είναι εκτός εμβέλειας.';

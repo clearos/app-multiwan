ccec9a37ea0ccbef2af9a9f8c1df8de15e6e16bb
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 16:46:54 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/fa/multiwan_lang.php
@@ -0,0 +1,22 @@
+<?php
+
+$lang['multiwan_app_description'] = 'برنامه های چند شبکه گسترده اجازه می دهد تا شما را به اتصال سیستم خود را به اتصال به اینترنت چند. چند شبکه گسترده ارائه می دهد مزایای بسیاری به محیط نیاز به اتصال قابل اعتماد، از جمله تعادل بار، تبعیض نژادی بسته و failover خودکار.';
+$lang['multiwan_app_name'] = 'چند شبکه گسترده';
+$lang['multiwan_backup'] = 'پشتیبان گیری';
+$lang['multiwan_destination_port_rule'] = 'بندر مقصد قانون';
+$lang['multiwan_destination_port_rules'] = 'مقصد قوانین بندر';
+$lang['multiwan_in_use'] = 'در حال استفاده';
+$lang['multiwan_mode'] = 'حالت';
+$lang['multiwan_multiwan'] = 'چند شبکه گسترده';
+$lang['multiwan_multiwan_status'] = 'چند شبکه گسترده وضعیت';
+$lang['multiwan_network_status'] = 'وضعیت شبکه';
+$lang['multiwan_offline'] = 'آفلاین';
+$lang['multiwan_online'] = 'آنلاین';
+$lang['multiwan_primary'] = 'اولیه';
+$lang['multiwan_source_based_route'] = 'مبتنی بر منبع مسیر';
+$lang['multiwan_source_based_routes'] = 'مسیر مبتنی بر منبع';
+$lang['multiwan_standby'] = 'آماده به کار';
+$lang['multiwan_updating_status'] = 'چند شبکه گسترده به روز رسانی است اطلاعات وضعیت، این تنها چند ثانیه طول بکشد ...';
+$lang['multiwan_weight'] = 'وزن';
+$lang['multiwan_weight_invalid'] = 'وزن چند شبکه گسترده نامعتبر است.';
+$lang['multiwan_weight_out_of_range'] = 'چند شبکه گسترده وزن است خارج از محدوده.';

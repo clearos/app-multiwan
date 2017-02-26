89847ef721b61acfdb03abe084f03101e1608fcb
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 16:42:59 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/hi/multiwan_lang.php
@@ -0,0 +1,22 @@
+<?php
+
+$lang['multiwan_app_description'] = 'मल्टी-वान app आप कई इंटरनेट कनेक्शन के लिए अपने सिस्टम से कनेक्ट करने की अनुमति देता है। मल्टी-वान भार संतुलन, पैकेट अलगाव और विफलता स्वचालित सहित एक विश्वसनीय कनेक्शन, आवश्यकता के वातावरण के लिए कई लाभ प्रदान करता है।';
+$lang['multiwan_app_name'] = 'मल्टी-वान';
+$lang['multiwan_backup'] = 'बैकअप';
+$lang['multiwan_destination_port_rule'] = 'गंतव्य पोर्ट नियम';
+$lang['multiwan_destination_port_rules'] = 'गंतव्य पोर्ट नियम';
+$lang['multiwan_in_use'] = 'उपयोग में';
+$lang['multiwan_mode'] = 'मोड';
+$lang['multiwan_multiwan'] = 'मल्टी-वान';
+$lang['multiwan_multiwan_status'] = 'मल्टी-वान स्थिति';
+$lang['multiwan_network_status'] = 'नेटवर्क की स्थिति';
+$lang['multiwan_offline'] = 'ऑफलाइन';
+$lang['multiwan_online'] = 'ऑनलाइन';
+$lang['multiwan_primary'] = 'मुख्य';
+$lang['multiwan_source_based_route'] = 'स्रोत आधारित रूट';
+$lang['multiwan_source_based_routes'] = 'स्रोत आधारित मार्गों';
+$lang['multiwan_standby'] = 'समर्थन करना';
+$lang['multiwan_updating_status'] = 'मल्टी-वान स्थिति की जानकारी अद्यतन कर रहा है, यह केवल कुछ सेकंड ले जाएगा ...';
+$lang['multiwan_weight'] = 'वजन';
+$lang['multiwan_weight_invalid'] = 'मल्टी-वान वजन अमान्य है।';
+$lang['multiwan_weight_out_of_range'] = 'मल्टी-वान वजन सीमा से बाहर है।';

a003c3c7b16115b4bf400dacb00035ddbedb5401
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 16:41:00 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/ny/multiwan_lang.php
@@ -0,0 +1,22 @@
+<?php
+
+$lang['multiwan_app_description'] = 'The app NAC-WAN limakupatsani kugwirizana dongosolo lanu kugwirizana angapo Internet. Mipikisano WAN amapereka madalitso ambiri mapangidwe zofuna kugwirizana odalirika, kuphatikizapo katundu akusamala, paketi tsankho ndi zodziwikiratu failover.';
+$lang['multiwan_app_name'] = 'Mipikisano WAN';
+$lang['multiwan_backup'] = 'kubwerera';
+$lang['multiwan_destination_port_rule'] = 'Kopita Port Rule';
+$lang['multiwan_destination_port_rules'] = 'Kopita Port Malamulo';
+$lang['multiwan_in_use'] = 'ntchito';
+$lang['multiwan_mode'] = 'akafuna';
+$lang['multiwan_multiwan'] = 'Mipikisano WAN';
+$lang['multiwan_multiwan_status'] = 'Mipikisano WAN .Umo';
+$lang['multiwan_network_status'] = 'Udindo Network';
+$lang['multiwan_offline'] = 'olumikizidwa ku makina';
+$lang['multiwan_online'] = 'Online';
+$lang['multiwan_primary'] = 'chachikulu';
+$lang['multiwan_source_based_route'] = 'Source ofotokoza Route';
+$lang['multiwan_source_based_routes'] = 'Source ofotokoza Njira';
+$lang['multiwan_standby'] = 'Yembekezera';
+$lang['multiwan_updating_status'] = 'Mipikisano WAN ndi kasinthidwe udindo mudziwe, izi kokha masekondi angapo ...';
+$lang['multiwan_weight'] = 'Kunenepa';
+$lang['multiwan_weight_invalid'] = 'Mipikisano WAN kulemera ndi opanda pake.';
+$lang['multiwan_weight_out_of_range'] = 'Mipikisano WAN kulemera anachokera osiyanasiyana.';

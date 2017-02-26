80af0352f0b710cd99dca6a8d85bb54fb5018c18
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 16:41:55 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/fi/multiwan_lang.php
@@ -0,0 +1,22 @@
+<?php
+
+$lang['multiwan_app_description'] = 'Multi-WAN app voit liittää järjestelmään useita Internet-yhteyksiä. Multi-WAN tarjoaa monia etuja ympäristöissä luotettava liitäntä, kuten kuormituksen tasapainotus, paketti erottelu ja automaattinen vikasietoisuuden.';
+$lang['multiwan_app_name'] = 'Multi-WAN';
+$lang['multiwan_backup'] = 'Varmuuskopioida';
+$lang['multiwan_destination_port_rule'] = 'Destination Port Rule';
+$lang['multiwan_destination_port_rules'] = 'Destination Port Säännöt';
+$lang['multiwan_in_use'] = 'Käytössä';
+$lang['multiwan_mode'] = 'tila';
+$lang['multiwan_multiwan'] = 'Multi-WAN';
+$lang['multiwan_multiwan_status'] = 'Multi-WAN tila';
+$lang['multiwan_network_status'] = 'verkon tila';
+$lang['multiwan_offline'] = 'Poissa';
+$lang['multiwan_online'] = 'Online';
+$lang['multiwan_primary'] = 'ensisijainen';
+$lang['multiwan_source_based_route'] = 'Lähde-pohjainen Route';
+$lang['multiwan_source_based_routes'] = 'Lähde-pohjainen Reitit';
+$lang['multiwan_standby'] = 'Valmiustila';
+$lang['multiwan_updating_status'] = 'Multi-WAN päivittää tilatiedot, tämä kestää vain muutaman sekunnin ...';
+$lang['multiwan_weight'] = 'Paino';
+$lang['multiwan_weight_invalid'] = 'Multi-WAN paino on virheellinen.';
+$lang['multiwan_weight_out_of_range'] = 'Multi-WAN paino on alueen ulkopuolella.';

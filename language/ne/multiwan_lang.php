0ce633cc61b8b3a5cfe63387d187620622774808
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 16:46:22 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/ne/multiwan_lang.php
@@ -0,0 +1,22 @@
+<?php
+
+$lang['multiwan_app_description'] = 'बहु-वान अनुप्रयोग तपाईं धेरै इन्टरनेट जडान गर्न आफ्नो सिस्टम जडान गर्न अनुमति दिन्छ। बहु-वान लोड सन्तुलन, प्याकेट अलगाव र स्वचालित failover सहित एक विश्वसनीय जडान, आवश्यकता वातावरण धेरै फाइदा प्रदान गर्दछ।';
+$lang['multiwan_app_name'] = 'बहु-वान';
+$lang['multiwan_backup'] = 'जगेडा';
+$lang['multiwan_destination_port_rule'] = 'गन्तव्य पोर्ट नियम';
+$lang['multiwan_destination_port_rules'] = 'गन्तव्य पोर्ट नियम';
+$lang['multiwan_in_use'] = 'प्रयोगमा';
+$lang['multiwan_mode'] = 'मोड';
+$lang['multiwan_multiwan'] = 'बहु-वान';
+$lang['multiwan_multiwan_status'] = 'बहु-वान स्थिति';
+$lang['multiwan_network_status'] = 'नेटवर्क स्थिति';
+$lang['multiwan_offline'] = 'अफलाइन';
+$lang['multiwan_online'] = 'अनलाइन';
+$lang['multiwan_primary'] = 'प्राथमिक';
+$lang['multiwan_source_based_route'] = 'स्रोत-आधारित मार्ग';
+$lang['multiwan_source_based_routes'] = 'स्रोत-आधारित मार्गहरू';
+$lang['multiwan_standby'] = 'तयारीमा बस्नू';
+$lang['multiwan_updating_status'] = 'बहु-वान स्थिति जानकारी अद्यावधिक छ, यो मात्र केहि सेकेन्ड हुनेछ ...';
+$lang['multiwan_weight'] = 'वजन';
+$lang['multiwan_weight_invalid'] = 'बहु-वान वजन अमान्य छ।';
+$lang['multiwan_weight_out_of_range'] = 'बहु-वान वजन दायरा बाहिर छ।';

6b7f1df2f2b414e0ee4f6acaf801f8aa65327980
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 16:45:09 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/lv/multiwan_lang.php
@@ -0,0 +1,22 @@
+<?php
+
+$lang['multiwan_app_description'] = 'Multi-WAN app ļauj savienot savu sistēmu ar vairākām interneta pieslēgumu. Multi-WAN piedāvā daudzas priekšrocības vidē prasa uzticamu savienojumu, ieskaitot balansēšanu, pakešu segregācijas un automātisko failover.';
+$lang['multiwan_app_name'] = 'Multi-WAN';
+$lang['multiwan_backup'] = 'rezerves';
+$lang['multiwan_destination_port_rule'] = 'Galamērķis Port noteikums';
+$lang['multiwan_destination_port_rules'] = 'Galamērķis Ostu Noteikumi';
+$lang['multiwan_in_use'] = 'in Use';
+$lang['multiwan_mode'] = 'mode';
+$lang['multiwan_multiwan'] = 'Multi-WAN';
+$lang['multiwan_multiwan_status'] = 'Multi-WAN Status';
+$lang['multiwan_network_status'] = 'tīkla statuss';
+$lang['multiwan_offline'] = 'Bezsaistē';
+$lang['multiwan_online'] = 'Online';
+$lang['multiwan_primary'] = 'Primārs';
+$lang['multiwan_source_based_route'] = 'Source bāzes Route';
+$lang['multiwan_source_based_routes'] = 'Avots bāzes maršruti';
+$lang['multiwan_standby'] = 'Stāvēt';
+$lang['multiwan_updating_status'] = 'Multi-WAN atjaunina statusa informāciju, tas aizņems tikai dažas sekundes ...';
+$lang['multiwan_weight'] = 'svars';
+$lang['multiwan_weight_invalid'] = 'Multi-WAN svars ir nederīgs.';
+$lang['multiwan_weight_out_of_range'] = 'Multi-WAN svars ir ārpus intervāla.';
